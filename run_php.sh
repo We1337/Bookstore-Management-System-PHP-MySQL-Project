726599137fe0559a4b45ba016bc5b4a84557379e
We1337 <githubdevice@gmail.com>
2023-05-07 04:00:03 +0500

    chore: port changed 8080 to 8000 test perpose

--- run_php.sh
+++ run_php.sh
@@ -1,2 +1,2 @@
 echo "PHP server up"
-php -S localhost:8080 -t "/home/we1337/Projects/Bookstore-Management-System-PHP-MySQL-Project/src/"
+php -S localhost:8000 -t "/home/we1337/Projects/Bookstore-Management-System-PHP-MySQL-Project/src/"
