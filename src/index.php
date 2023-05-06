8c930ca55ac2426c6a5c7ba041fe58299e9da425
We1337 <githubdevice@gmail.com>
2023-05-07 04:01:21 +0500

    feat: header and footer section removed and created new files

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
     session_start();
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html class="dark">
     <head>
         <meta charset="UTF-8">
@@ -14,47 +14,7 @@
     </head>
     <body class="bg-gray-900 text-white p-4">
 
-        <nav class="border-gray-200 dark:bg-gray-900">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="index.php" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                </a>
-                <div class="flex md:order-2">
-                    <?php
-                        if (isset($_SESSION["username"]))
-                        {
-                            echo '
-                                <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                                    <span class="font-medium text-gray-600 dark:text-gray-300"><a href="includes/logout.inc.php">JB</a></span>
-                                </div>
-                            ';
-                        }
-                        else
-                        {
-                            echo '<button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>';
-                        }
-                        ?>
-                    </div>
-                    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-                        <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                            <li>
-                                <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
+        <?php include 'header.php'; ?> 
 
         <!-- Main modal -->
         <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="bg-gray-900 text-white p-4 fixed top-0 left-0 right-0 z-50 hidden w-full overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
@@ -76,69 +36,57 @@
                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
                                 <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
                             </div>
-                            <button name="submit" type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+                            <button name="submit" id="ClickTheButton" type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
                             <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                                Not registered? <a href="sign_up.php" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                                Not registered? <a href="signup.php" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
                             </div>
-                        </form>
-                        <?php
-                            if(isset($_GET["error"])) {
-                                if ($_GET["error"] == "emptyinput") {
-                                    echo '<p>Fill in all fields!</p>';
-                                }
-                                if ($_GET["error"] == "wronglogin") {
-                                    echo '<p>Invalid login credentials!</p>';
-                                }
-                            }
-                        ?>
+                        </form>  
                     </div>
                 </div>
             </div>
         </div> 
 
-        <div class="bg-gray-900 text-white p-4 relative flex flex-col items-center justify-center min-h-screen bg-cover min-w-screen">
-            <div class="flex flex-col items-center justify-center p-10 mx-auto lg:flex-row lg:max-w-6xl lg:p-0">
+        <!-- Alert windows -->
+        <div class="pl-5 pr-5">
+            <?php
+                if( isset($_GET["error"])) {
+                    if ( $_GET["error"] == "emptyinput") {
+                        echo '<div class="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 mg-4" role="alert">
+                            <span class="font-medium">Warning alert!</span> empty input, please try again.
+                            </div>';
+                    }
+                    if ( $_GET["error"] == "wronglogin") {
+                        echo '<div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                            <span class="font-medium">Danger alert!</span> wrong login, please try again.
+                            </div>';
+                    }
+                    if($_GET["error"]=="none") {
+                        echo '<div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+                            <span class="font-medium">Success alert!</span> You are signed up.
+                            </div>';
+                    }
+                }
+            ?>
+        </div>
+
+        <!-- Hero section -->
+        <div class="bg-gray-900 text-white relative flex flex-col items-center justify-center min-h-screen bg-cover min-w-screen">
+            <div class="flex flex-col items-center justify-center p-5 mx-auto lg:flex-row lg:max-w-6xl lg:p-0">
                 <div class="container relative z-20 flex flex-col w-full px-5 pb-1 pr-12 mb-16 text-2xl text-gray-700 lg:w-1/2 sm:px-0 md:px-10 sm:items-center lg:items-start lg:mb-0">
-                    <h1 class="relative z-20 text-5xl font-extrabold leading-none text-purple-500 xl:text-6xl sm:text-center lg:text-left">Designed with<br class="md:hidden lg:block"> <span class="text-white">you in mind</span></h1>
+                    <h1 class="relative z-20 text-5xl font-extrabold leading-none text-purple-500 xl:text-6xl sm:text-center lg:text-left">Bookstore<br class="md:hidden lg:block"> <span class="text-white">ONLINE</span></h1>
                     <p class="relative z-20 block mt-6 text-base text-gray-500 xl:text-xl sm:text-center lg:text-left">We've crafted the ultimate design tool that will help you build the design of your dreams. Built with configuration in mind, you can fully customize every aspect.</p>
                     <div class="relative flex mt-4"></div>
                 </div>
                 <div class="relative w-full px-5 rounded-lg md:w-2/3 lg:w-1/2 group xl:px-0">
                     <div class="relative overflow-hidden rounded-md shadow-2xl">
-                        <img src="https://images.unsplash.com/photo-1493857671505-72967e2e2760?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&h=1603&q=80" class="z-10 object-cover w-full h-full">
+                        <img src="https://hips.hearstapps.com/hmg-prod/images/gettyimages-502579466-1582919962.jpg" class="z-10 object-cover w-full h-full">
                     </div>
                 </div>
             </div>
         </div>
 
-        <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-            <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                <div class="sm:flex sm:items-center sm:justify-between">
-                    <a href="index.php" class="flex items-center mb-4 sm:mb-0">
-                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                    </a>
-                    <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                        <li>
-                            <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-                        </li>
-                        <li>
-                            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="index.php" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
-            </div>
-        </footer>
+        <?php include 'footer.php'; ?>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
-
     </body>
 </html>
