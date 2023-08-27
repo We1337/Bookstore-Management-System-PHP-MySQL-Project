dc7d9645928a52913718a3322e1cb3ac7aedb0b9
We1337 <githubdevice@gmail.com>
2023-08-27 08:56:42 +0500

    feater: index copy

---
+++ src/index_copy.php
@@ -0,0 +1,18 @@
+<?php
+    include("includes/header.php");
+?>
+
+<div id="content">
+    <div class="post">
+        <h2 class="title"><a href="#">Welcome to Bookstore Management System</a></h2>
+        <p class="meta"></p>
+        <div class="entry">
+            Your Design
+        </div>
+    </div>
+</div><!-- end #content -->
+
+<?php
+    include("includes/footer.php");
+?>
+    
