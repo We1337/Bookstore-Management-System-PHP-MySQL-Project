08ded483c5b7c113a41566ae47bfa008933ac192
We1337 <githubdevice@gmail.com>
2023-08-30 21:36:14 +0600

    feat: restyle of the page

--- forget_password.php
+++ forget_password.php
@@ -2,62 +2,65 @@
 	include("includes/header.php");
 ?>
 
-<div id="content">
-	<div class="post">
-		<h2 class="title"><a href="#">Forget Password</a></h2>
-		<p class="meta"></p>
-		<div class="entry">
-				
-			<?php
-				if(!empty($_SESSION['error']))
-				{
-					foreach($_SESSION['error'] as $er)
-					{
-						echo '<font color="red">'.$er.'</font><br>';
-					}
-					unset($_SESSION['error']);
-				}
-			?>
+<div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content rounded-4 shadow">
+			<div class="modal-header p-5 pb-4 border-bottom-0">
+				<h1 class="fw-bold mb-0 fs-2">Forget Password</h1>
+			</div>
 
-			<form class="login" action="forget_password_process.php" method="post">
+			<div class="modal-body p-5 pt-0">
+				<form action="forget_password_process.php" method="POST">
+
+					<div class="form-floating mb-3">
+            			<input name="unm" type="text" class="form-control rounded-3" placeholder="Full Name">
+            			<label>User Name</label>
+						<?php 
+							if(isset($_SESSION['error']['unm']))
+							{
+								echo '<font color="red">'.$_SESSION['error']['unm'].'</font>';
+							}
+						?>
+					</div>
+
+					<select class="form-select" name="question" aria-label="Default select example">
+  						<option>Which is your Favourite Movie?</option>
+  						<option>Which is your Favourite Actress?</option>
+					</select>
+					<?php
+						if(isset($_SESSION['error']['que']))
+						{
+							echo '<font color="red">'.$_SESSION['error']['que'].'</font>';
+						}
+					?>
 					
-				User Name :<br>
-				<?php 
-					if(isset($_SESSION['error']['unm']))
-					{
-						echo '<font color="red">'.$_SESSION['error']['unm'].'</font>';
-					}
-				?>
-				<input type="text" name="unm" class="txt"><br><br>
+					<div class="form-floating mb-3">
+            			<input name="pwd" type="password" class="form-control rounded-3" placeholder="Password">
+            			<label>Password</label>
+						<?php
+							if(isset($_SESSION['error']['pwd']))
+							{
+								echo '<font color="red">'.$_SESSION['error']['pwd'].'</font>';
+							}
+						?>
+          			</div>
 
-				Security Question :<br>
-				<select name="question" class="txt">
-					<option>Which is your Favourite Movie ?</option>
-					<option>What is your Favourite A/ctress ?<option>
-				</select>
-				<br><br>
+					<div class="form-floating mb-3">
+            			<input name="cpwd" type="password" class="form-control rounded-3" placeholder="Confirm Password">
+            			<label>Confirm Password</label>
+						<?php
+							if(isset($_SESSION['error']['pwd']))
+							{
+								echo '<font color="red">'.$_SESSION['error']['pwd'].'</font>';
+							}
+						?>
+          			</div>
 
 
-				Security Answer :<br>
-				<input type="text" name="answer" class="txt">
-				<?php 
-					if(isset($_SESSION['error']['answer']))
-					{
-						echo '<font color="red">'.$_SESSION['error']['answer'].'</font>';
-					}
-				?>
-				<br><br>
+					<input type="submit" class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" value="Change Password">
 
-				New Password :<br>
-				<input type="text" name="pwd" class="txt">
-				<br><br>
-
-				Confirm New Password :<br>
-				<input type="password" name="cpwd" class="txt"><br><br>
-
-				<input type="submit" class="btn" value="Change Password">
-
-			</form>
+				</form>
+			</div>
 		</div>
 	</div>
 </div><!-- end #content -->
