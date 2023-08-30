4734c89d9f2cfbefc7791f4b446ab625311a26cf
We1337 <githubdevice@gmail.com>
2023-08-30 22:01:32 +0600

    feat: restyle of the page

--- order.php
+++ order.php
@@ -1,59 +1,99 @@
 <?php
 	include("includes/header.php");
-
-	include("includes/connection.php");
 ?>
 
-<div id="content">
-	<div class="post">
-		<h2 class="title"><a href="#">Order - Cash On Delivery</a></h2>
-		<p class="meta"></p>
-		<div class="entry">	
-			<form style="color: black" role="form" action="order_process.php" class="register" method="post">
-					
-				<?php
-                    if(isset($_GET['order']))
-                    {
-                        echo '<font style="color:red">Order Successfully Placed</font><br><br>';
-                    }
-                ?>
+<div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSignin">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content rouneded-4 shadow">
+			<div class="modal-header p-5 pb-4 border-bottom-0">
+				<h1 class="fw-bold mb-0 fs-2">Order - Cash On Delivery</h1>
+			</div>
 
-                <?php
-					if(!empty($_SESSION['error']))
-					{
-						foreach($_SESSION['error'] as $er)
-						{
-							echo '<font color="red">'.$er.'</font><br>';
-						}
-						unset($_SESSION['error']);
-					}
-				?>
-				<br>
-
-				Full Name :<br>
-				<input type="text" name="fnm" class="txt" placeholder="First Name or Surname"><br><br>
-
-				Address :<br>
-				<input type="text" name="add" class="txt" placeholder="Address"><br><br>
-
-				Pin Code :<br>
-				<input type="text" name="pc" class="txt" placeholder="City Pin Code"><br><br>
-
-				City :<br>
-				<input type="text" name="city" class="txt" placeholder="City"><br><br>
-
-				State :<br>
-				<input type="text" name="state" class="txt" placeholder="State"><br><br>
-
-				Mobile Number :<br>
-  				<input type="text" name="mno" class="txt" placeholder="Mobile Number"><br><br>
-
-				<button type="submit" name="sub" class="btn btn-default">Confirm & Proceed</button>
+			<div class="modal-body p-5 pt-0">	
+				<form action="order_process.php" method="POST">
 						
-			</form>
+					<?php
+						if(isset($_GET['order']))
+						{
+							echo '<font style="color:red">Order Successfully Placed</font><br><br>';
+						}
+					?>
+
+					<br>
+
+					<div class="form-floating mb-3">
+						<input name="fnm" type="text" class="form-control rounded-3" placeholder="Full Name">
+						<label>Full Name or Surname</label>
+						<?php	
+							if(isset($_SESSION['error']['fnm']))
+							{
+								echo '<font color="red">'.$_SESSION['error']['fnm'].'</font>';
+							}
+						?>
+					</div>
+
+					<div class="form-floating mb-3">
+						<input name="add" type="text" class="form-control rounded-3" placeholder="Address">
+						<label>Address</label>
+						<?php
+							if(isset($_SESSION['error']['add']))
+							{
+								echo '<font color="red">'.$_SESSION['error']['add'].'</font>';
+							}
+						?>
+					</div>
+
+					<div class="form-floating mb-3">
+						<input name="pc" type="text" class="form-control rounded-3" placeholder="Pin code">
+						<label>Pin code</label>
+						<?php
+							if(isset($_SESSION['error']['pc']))
+							{
+								echo '<font color="red">'.$_SESSION['error']['add'].'</font>';
+							}
+						?>
+					</div>
+					
+					<div class="form-floating mb-3">
+						<input name="city" type="text" class="form-control rounded-3" placeholder="City">
+						<label>City</label>
+						<?php
+							if(isset($_SESSION['error']['city']))
+							{
+								echo '<font color="red">'.$_SESSION['error']['city'].'</font>';
+							}
+						?>
+					</div>
+
+					<div class="form-floating mb-3">
+						<input name="state" type="text" class="form-control rounded-3" placeholder="State">
+						<label>State</label>
+						<?php
+							if(isset($_SESSION['error']['state']))
+							{
+								echo '<font color="red">'.$_SESSION['error']['state'].'</font>';
+							}
+						?>
+					</div>
+
+					<div class="form-floating mb-3">
+						<input name="mno" type="text" class="form-control rounded-3" placeholder="Mobile Number">
+						<label>Mobile Number</label>
+						<?php
+							if(isset($_SESSION['error']['mno']))
+							{
+								echo '<font color="red">'.$_SESSION['error']['state'].'</font>';
+							}
+						?>
+					</div>
+
+					<button type="submit" name="sub" class="btn btn-outline-primary">Confirm & Proceed</button>
+							
+				</form>
+			</div>
 		</div>
 	</div>
-</div><!-- end #content -->
+</div>
 
 <?php
 	include("includes/footer.php");
