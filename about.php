1bee481f148129a4f567e70fc20b07bda64f5909
We1337 <githubdevice@gmail.com>
2023-08-31 16:09:48 +0600

    feat: new page about updated with new style

--- about.php
+++ about.php
@@ -1,3 +1,17 @@
 <?php
-    echo 'About';
+	include("includes/header.php");
+?>
+    
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+
+        <div class="py-5 text-center">
+            <img class="d-block mx-auto mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+            <h2>Checkout form</h2>
+            <p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>        
+
+    </div>
+
+<?php
+    include("includes/footer.php");
 ?>
