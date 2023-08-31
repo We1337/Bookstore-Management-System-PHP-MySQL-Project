45f0aa230455b1097c9b33f9f803ac7f6a1d5d20
We1337 <githubdevice@gmail.com>
2023-08-31 13:37:35 +0600

    feat: restyle of the page detail

--- book_detail.php
+++ book_detail.php
@@ -11,84 +11,31 @@
 	$book_row=mysqli_fetch_assoc($book_res);
 ?>
 
-<div id="content">
-	<div class="post">
+	<header class="d-flex justify-content-center py-3">
+        <h3 class="nav-item"><?php echo $book_row['cat_nm']; ?></h3>
+    </header>
 
-		<header class="d-flex justify-content-center py-3">
-        	<h3 class="nav-item"><?php echo $book_row['cat_nm']; ?></h3>
-    	</header>
-
-
-		<div class="container px-4 py-5" id="custom-cards">
-    		<h2 class="pb-2 border-bottom">Custom cards</h2>
-
-    		<div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-      			<div class="col">
-        			<div class="card card-cover h-100 text-bg-dark shadow-lg" style="background: url('<?php echo $book_row['b_img']; ?>');">
-          				<div class="d-flex flex-column h-100 p-100 pb-3 text-shadow-1">
-            
-          				</div>
-        			</div>
-      			</div>
-    		</div>
-
-  		</div>
-
-
-
-		<div class="entry">
-			<table class="book_detail" width="100%" border="0px">
-				<tr valign="top">
-					<td width="48%">
-						<img class="book_img" src="" width="280px" height="350px">
-					</td>
-
-					<td>
-						<h1><?php echo $book_row['b_nm']; ?></h1>
-
-						<p class="desc"><?php echo $book_row['b_desc']; ?></p>
-
-						<p class="price">Rs. <?php echo $book_row['b_price']; ?></p>
-
-						<?php
-
-							$is_cart = 0;
-
-							if(isset($_SESSION['cart']))
-							{
-								foreach($_SESSION['cart'] as $id => $val)
-								{
-									if($val['img'] == $book_row['b_img'])
-									{	
-										$is_cart = 1;
-										break;
-									}
-								}
-							}
-
-							if(isset($_SESSION['client']['status']))
-							{
-								if($is_cart == 0)
-								{
-									echo '<a href="addtocart.php?bcid='.$book_row['b_id'].'" class="cart_btn">Add to Cart</a>';
-								}
-								else
-								{
-									echo "Already in Cart";
-								}
-							}
-							else
-							{
-								echo '<a href="#" class="cart_btn">Add to Cart</a><a style="text-decoration: none" href="login.php"><h2>Click here Login..</h2></a>';
-							}
-						?>
-					</td>
-				</tr>
-			</table>
-		</div>
-	</div>
-</div><!-- end #content -->
-
+	<div class="container text-center"> <!-- Center the content -->
+        <div class="d-flex justify-content-center py-3">
+            <div class="col-md-12"> <!-- Increase the column width -->
+                <div class="card flex-md-row mb-4 box-shadow h-md-550"> <!-- Increase the card height -->
+                    <div class="card-body d-flex flex-column align-items-start">
+                        <strong class="d-inline-block mb-2 text-primary"><?php echo $book_row['b_nm']; ?></strong>
+                        <h3 class="mb-0">
+                            <?php echo $book_row['b_price']; ?> KZT
+                        </h3>
+                        <p class="card-text mb-auto">
+							<?php echo $book_row['b_desc']; ?>
+						</p>
+                        <a href="#">Continue reading</a>
+						<a href="#">COntinue reading</a>
+                    </div>
+                    <img class="card-img-right flex-auto d-none d-md-block" alt="Thumbnail [500x550]" style="width: 500px; height: 550px;" src="<?php echo $book_row['b_img']; ?>" data-holder-rendered="true">
+                </div>
+            </div>
+        </div>
+    </div>
+	
 <?php
 	include("includes/footer.php");
 ?>
