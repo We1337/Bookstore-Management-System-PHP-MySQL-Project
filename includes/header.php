d8c1d048cc389b062860e6c77d7f50a0388c3e5f
We1337 <githubdevice@gmail.com>
2023-08-31 15:18:19 +0600

    feat: new button style

--- includes/header.php
+++ includes/header.php
@@ -27,10 +27,10 @@
         			</a>
 
         			<ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          				<li><a href="index.php" class="nav-link px-2 text-secondary">Home</a></li>
+          				<li><a href="index.php" class="px-2 btn btn-outline-light">Home</a></li>
           				<li><a href="contact.php" class="nav-link px-2 text-white">Contact Us</a></li>
           				<li><a href="cart.php" class="nav-link px-2 text-white">Cart</a></li>
-          				<li><a href="#" class="nav-link px-2 text-white">About</a></li>
+          				<li><a href="about.php" class="nav-link px-2 text-white">About</a></li>
         			</ul>
 
 					<form method="get" action="search.php" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3 d-flex align-items-center" role="search">
