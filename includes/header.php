ca7baf32c1f72879d602d4d796966bc4de80338e
We1337 <githubdevice@gmail.com>
2023-08-31 16:14:26 +0600

    fix: removed logo unused logo

--- includes/header.php
+++ includes/header.php
@@ -22,14 +22,11 @@
 		<header class="p-3 text-bg-dark">
     		<div class="container">
       			<div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        			<a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          				<svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-        			</a>
 
         			<ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           				<li><a href="index.php" class="px-2 btn btn-outline-light">Home</a></li>
-          				<li><a href="contact.php" class="nav-link px-2 text-white">Contact Us</a></li>
-          				<li><a href="cart.php" class="nav-link px-2 text-white">Cart</a></li>
+						<li><a href="cart.php" class="nav-link px-2 text-white">Cart</a></li>
+          				<li><a href="contact.php" class="nav-link px-2 text-white">Contact us</a></li>
           				<li><a href="about.php" class="nav-link px-2 text-white">About</a></li>
         			</ul>
 
