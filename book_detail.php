4dc125e73ada509a6602cdcbe417dac2e252a151
We1337 <githubdevice@gmail.com>
2023-08-31 15:17:23 +0600

    feat: book details

--- book_detail.php
+++ book_detail.php
@@ -4,7 +4,7 @@
 
 	$bid=$_GET['id'];
 
-	$book_query="SELECT * FROM book,category WHERE b_cat=cat_id AND b_id=$bid";
+	$book_query="SELECT * FROM book,category WHERE b_cat = cat_id AND b_id = $bid";
 
 	$book_res=mysqli_query($link, $book_query);
 
@@ -27,8 +27,44 @@
                         <p class="card-text mb-auto">
 							<?php echo $book_row['b_desc']; ?>
 						</p>
-                        <a href="#">Continue reading</a>
-						<a href="#">COntinue reading</a>
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+
+                                <?php
+
+								$is_cart = 0;
+
+								if(isset($_SESSION['cart']))
+								{
+									foreach($_SESSION['cart'] as $id => $val)
+									{
+										if($val['img'] == $book_row['b_img'])
+										{	
+											$is_cart = 1;
+											break;
+										}
+									}
+								}
+
+								if(isset($_SESSION['client']['status']))
+								{
+									if($is_cart == 0)
+									{
+										echo '<a href="addtocart.php?bcid='.$book_row['b_id'].'" class="btn btn-outline-success mb-3">Add to Cart</a>';
+									}
+									else
+									{
+										echo "Already in Cart";
+									}
+								}
+								else
+								{
+                                    echo '<a type="button" href="login.php" class="btn btn-outline-success me-2">Click here Login..</a>';
+								}
+							?>
+
+                            </div>
+                        </div>
                     </div>
                     <img class="card-img-right flex-auto d-none d-md-block" alt="Thumbnail [500x550]" style="width: 500px; height: 550px;" src="<?php echo $book_row['b_img']; ?>" data-holder-rendered="true">
                 </div>
