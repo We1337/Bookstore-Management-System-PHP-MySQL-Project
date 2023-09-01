f2f7a45866e78503c165e4abf6058e946d45b411
We1337 <githubdevice@gmail.com>
2023-09-01 09:36:00 +0600

    feat: admin has all result output

--- admin/index.php
+++ admin/index.php
@@ -2,10 +2,21 @@
     include("includes/header.php");
     include("../includes/connection.php");
 
-    $count = "SELECT count(*) AS total FROM register;";
-    $res = mysqli_query($link, $count);
-    $relres =  mysqli_fetch_assoc($res);
-    
+    $countUsers = "SELECT count(*) AS total FROM `register`;";
+    $resUsers = mysqli_query($link, $countUsers);
+    $realResultUsers =  mysqli_fetch_assoc($resUsers);
+   
+    $countBooks = "SELECT count(*) AS total FROM `book`;";
+    $resBooks = mysqli_query($link, $countBooks);
+    $realResultBooks = mysqli_fetch_assoc($resBooks);
+
+    $countOrder = "SELECT count(*) AS total FROM `order`;";
+    $resOrder = mysqli_query($link, $countOrder);
+    $realResultOrder = mysqli_fetch_assoc($resOrder);
+
+    $countContact = "SELECT count(*) AS total FROM `contact`;";
+    $resContact = mysqli_query($link, $countContact);
+    $realResultContact = mysqli_fetch_assoc($resContact);
 ?>
        
     <main role="main" class="container">
@@ -14,18 +25,23 @@
             <h6 class="border-bottom border-gray pb-2 mb-0">Data base information</h6>
             <div class="media pt-3">
                 <h4 class="media-body pb-3 mb-0 lh-125 border-bottom border-gray">
-                    <strong> Users: <?php echo $relres['total']; ?></strong>
+                    <strong>Users: <?php echo $realResultUsers['total']; ?></strong>
                 </h4>
             </div>
-            <div class="media text-muted pt-3">
-                <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-                </p>
+            <div class="media pt-3">
+                <h4 class="media-body pb-3 mb-0 lh-125 border-bottom border-gray">
+                    <strong>Books: <?php echo $realResultBooks['total']; ?></strong>
+                </h4>
             </div>
-            <div class="media text-muted pt-3">
-                <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-                </p>
+            <div class="media pt-3">
+                <h4 class="media-body pb-3 mb-0 lh-125 border-bottom border-gray">
+                    <strong>Orders: <?php echo $realResultOrder['total']; ?></strong>
+                </h4>
+            </div>
+            <div class="media pt-3">
+                <h4 class="media-body pb-3 mb-0 lh-125 border-bottom border-gray">
+                    <strong>Contact: <?php echo $realResultContact['total']; ?></strong>
+                </h4>
             </div>
         </div>
 
