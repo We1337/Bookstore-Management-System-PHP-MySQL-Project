cf68e03140996d576e891e7d46cd25baa8725135
We1337 <githubdevice@gmail.com>
2023-09-01 08:57:03 +0600

    feat: index page style changed to new

--- admin/index.php
+++ admin/index.php
@@ -1,39 +1,37 @@
 <?php
     include("includes/header.php");
-
     include("../includes/connection.php");
-?>
-        <div id="page-wrapper">
-            
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
 
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <h1 class="page-header">&nbsp;Admin</h1>
-                            </div>
-                            <!-- /.col-lg-12 -->
-                        </div>
-                        <div class="panel-heading">
-                            Welcome admin
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                
-                            </div>
-                            <!-- /.table-responsive -->
-                            
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
+    $count = "SELECT count(*) AS total FROM register;";
+    $res = mysqli_query($link, $count);
+    $relres =  mysqli_fetch_assoc($res);
     
+?>
+       
+    <main role="main" class="container">
+
+        <div class="my-3 p-3 bg-white rounded box-shadow">
+            <h6 class="border-bottom border-gray pb-2 mb-0">Data base information</h6>
+            <div class="media pt-3">
+                <h4 class="media-body pb-3 mb-0 lh-125 border-bottom border-gray">
+                    <strong> Users: <?php echo $relres['total']; ?></strong>
+                </h4>
+            </div>
+            <div class="media text-muted pt-3">
+                <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                </p>
+            </div>
+            <div class="media text-muted pt-3">
+                <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                </p>
+            </div>
         </div>
-        <!-- /#page-wrapper -->
+
+    </main>
+
+       
 <?php
     include("includes/footer.php");
 ?>
