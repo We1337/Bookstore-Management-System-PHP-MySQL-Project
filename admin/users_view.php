463eca3750dca7e467ba1b2f416b3148a84166cb
We1337 <githubdevice@gmail.com>
2023-09-01 12:40:23 +0600

    style: remove comments

--- admin/users_view.php
+++ admin/users_view.php
@@ -7,16 +7,13 @@
                 <div class="col-lg-12">
                     <h1 class="page-header">View Users</h1>
                 </div>
-                <!-- /.col-lg-12 -->
             </div>
-            <!-- /.row -->
             <div class="row">
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Book List
                         </div>
-                        <!-- /.panel-heading -->
                         <div class="panel-body">
                             <div class="table-responsive">
                                 <table class="table table-striped table-bordered table-hover" id="dataTables-example">
@@ -62,16 +59,12 @@
                                     </tbody>
                                 </table>
                             </div>
-                            <!-- /.table-responsive -->
                         </div>
-                        <!-- /.panel-body -->
                     </div>
-                    <!-- /.panel -->
                 </div>
-                <!-- /.col-lg-12 -->
             </div> 
         </div>
-        <!-- /#page-wrapper -->
+
 <?php
     include("includes/footer.php");
 ?>
