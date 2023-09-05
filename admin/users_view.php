dbe68c78955f720ad94d07e7629ff013efdafb9d
We1337 <githubdevice@gmail.com>
2023-09-05 19:08:05 +0600

    feat: restyle page user_view

--- admin/users_view.php
+++ admin/users_view.php
@@ -2,66 +2,59 @@
     include("includes/header.php");
     include("../includes/connection.php");
 ?>
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">View Users</h1>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Book List
-                        </div>
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                    <thead>
-                                        <tr>
-                                            <th>No</th>
-                                            <th>Register ID</th>
-                                            <th>Full Name</th>
-                                            <th>User Name</th>
-                                            <th>Contact No</th>
-                                            <th>E-Mail</th>
-                                            <th>Register Date</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+        <div class="container">
+            <div class="post">
+                <br>
+                <h2 class="title text-center">View Users</h2>
 
-                                        <?php
+                <form method="GET" action="contact_view.php" class="form-inline">
+                    <input class="form-control mr-sm-2" type="text" name="s" placeholder="Search" aria-label="Search">
+                    <br>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="search-submit">Search</button>
+                </form> 
+                <br>
 
-                                            $r_q = "SELECT * FROM register";
+                <table class="table table-striped table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Register ID</th>
+                            <th scope="col">Full Name</th>
+                            <th scope="col">User Name</th>
+                            <th scope="col">Contact No</th>
+                            <th scope="col">E-Mail</th>
+                            <th scope="col">Register Date</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-                                            $r_res = mysqli_query($link, $r_q);
+                        <?php
+                            $s = $_GET['s'];
+                            $blq = "SELECT * FROM register WHERE r_fnm LIKE '%$s%'";
+                            $blres = mysqli_query($link, $blq);
 
-                                            $count = 1;
+                            $count = 1;
 
-                                            while($r_row = mysqli_fetch_assoc($r_res))
-                                            {
-                                                echo '
-                                                    <tr class="odd gradeX">
-                                                        <td>'.$count.'</td>
-                                                        <td>'.$r_row['r_id'].'</td>
-                                                        <td>'.$r_row['r_fnm'].'</td>
-                                                        <td>'.$r_row['r_unm'].'</td>
-                                                        <td>'.$r_row['r_cno'].'</td>
-                                                        <td>'.$r_row['r_email'].'</td>
-                                                        <td>'.@date("d-M-y",$book_row['r_time']).'</td>
-                                                        <td align="center"><a style="color: red;" href="process_users_del.php?id='.$r_row['r_id'].'">x</a></td>
-                                                    </tr>';
-                                                $count++;
-                                            }
-                                        ?>
+                            while($r_row = mysqli_fetch_assoc($blres))
+                            {
+                                echo '
+                                    <tr>
+                                        <td>'.$count.'</td>
+                                        <td>'.$r_row['r_id'].'</td>
+                                        <td>'.$r_row['r_fnm'].'</td>
+                                        <td>'.$r_row['r_unm'].'</td>
+                                        <td>'.$r_row['r_cno'].'</td>
+                                        <td>'.$r_row['r_email'].'</td>
+                                        <td>'.@date("d-M-y",$book_row['r_time']).'</td>
+                                        <td align="center"><a style="color: red;" href="process_users_del.php?id='.$r_row['r_id'].'">x</a></td>
+                                    </tr>';
+                                $count++;
+                            }
+                        ?>
                                             
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    </tbody>
+                </table>
             </div> 
         </div>
 
