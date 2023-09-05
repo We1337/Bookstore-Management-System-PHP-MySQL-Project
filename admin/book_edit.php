5a37614aade6baa06958d030717006383680d3d0
We1337 <githubdevice@gmail.com>
2023-09-05 10:05:13 +0600

    feat: restyle page

--- admin/book_edit.php
+++ admin/book_edit.php
@@ -2,114 +2,103 @@
     include("includes/header.php");
     include("../includes/connection.php");
 
-    $cq = "SELECT * FROM book WHERE b_id = ".$_GET['id'];
+    $cq = "SELECT * FROM book WHERE b_id = " . $_GET['id'] . ";";
 
     $res = mysqli_query($link, $cq);
 
     $crow = mysqli_fetch_assoc($res);
 ?>
 
-<div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSignin">
-  	<div class="modal-dialog" role="document">
-    	<div class="modal-content rounded-4 shadow">
-            <div class="modal-header p-5 pb-4 border-bottom-0">
-                <h1 class="page-header text-center">Update Book</h1>
-            </div>
-        
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Edit Book
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <form role="form" action="process_book_edit.php" method="post" enctype="multipart/form-data">
-
-                                        <input type="hidden" name="id" value="<?php echo $crow['b_id']; ?>" />
-                                        <div class="form-group">
-                                            <label>Book Name</label>
-                                                <?php
-                                                    if(isset($_SESSION['error']['bnm']))
-                                                    {
-                                                        echo '<p class="error">'.$_SESSION['error']['bnm'].'</p>';
-                                                    } 
-                                                ?>
-                                            <input type="text" name="bnm" value="<?php echo $crow['b_nm'] ?>" class="form-control">
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label>Book Category</label>
-                                            <select name="cat" class="form-control">
-                                                <?php
-                                                    include("../includes/connection.php");
-
-                                                    $cq = "SELECT * FROM category";
-
-                                                    $cres = mysqli_query($link, $cq);
-
-                                                    while($crow = mysqli_fetch_assoc($cres))
-                                                    {
-                                                        echo '<option value="'.$crow['cat_id'].'">'.$crow['cat_nm'].'</option>';
-                                                    }
-                                                ?>
-                                            </select>
-                                        </div>
- 
-                                        <div class="form-group">
-                                            <label>Description
-                                                <?php
-                                                    if(isset($_SESSION['error']['desc']))
-                                                    {
-                                                        echo '<p class="error">'.$_SESSION['error']['desc'].'</p>';
-                                                    }
-                                                ?>
-                                            </label>
-                                            <textarea name="desc" rows="3" class="form-control">
-                                                 <?php echo $crow['b_desc'] ?>
-                                            </textarea>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label>Price</label>
-                                                <?php
-                                                    if(isset($_SESSION['error']['price']))
-                                                    {
-                                                        echo '<p class="error">'.$_SESSION['error']['price'].'</p>';
-                                                    } 
-                                                ?>
-                                            <input type="text" name="price" value="<?php echo $crow['b_price'] ?>" class="form-control">
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label>Book Image</label>
-                                                <?php
-                                                    if(isset($_SESSION['error']['b_img']))
-                                                    {
-                                                        echo '<p class="error">'.$_SESSION['error']['b_img'].'</p>';
-                                                    }
-                                                ?>
-                                            <input type="file" name="b_img" class="form-control">
-                                        </div>
-
-                                        <button type="submit" class="btn btn-default">Update Book</button>
-
-                                        <a href="book_view.php" class="btn btn-default">Exit</a>
-
-                                    </form>
-
-                                    <?php
-                                        unset($_SESSION['error']);
-                                    ?>
-
-                                </div>
-                            </div>
-                        </div>
+        <div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSignin">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content rounded-4 shadow">
+                    <div class="modal-header p-5 pb-4 border-bottom-0">
+                        <h1 class="page-header text-center">Update Book</h1>
                     </div>
+        
+                    <div class="modal-body p-5 pt-0">
+
+                        <form role="form" action="process_book_edit.php" method="POST" enctype="multipart/form-data">
+
+                            <input type="hidden" name="id" value="<?php echo $crow['b_id']; ?>" />
+
+                            <div class="form-floating mb-3">
+                                <input type="text" name="bnm" value="<?php echo $crow['b_nm'] ?>" class="form-control">
+                                <label>Book Name</label>
+                                    <?php
+                                        if(isset($_SESSION['error']['bnm']))
+                                        {
+                                            echo '<p class="error">'.$_SESSION['error']['bnm'].'</p>';
+                                        } 
+                                    ?>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <select name="cat" class="form-control">
+                                    <?php
+                                        include("../includes/connection.php");
+
+                                        $cq = "SELECT * FROM category";
+
+                                        $cres = mysqli_query($link, $cq);
+
+                                        while($crow = mysqli_fetch_assoc($cres))
+                                        {
+                                            echo '<option value="'.$crow['cat_id'].'">'.$crow['cat_nm'].'</option>';
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+    
+                            <div class="form-floating mb-3">
+                                <textarea name="desc" class="form-control">
+                                    <?php echo $crow['b_desc'] ?>
+                                </textarea>
+                                <label>Description</label>
+                                <?php
+                                    if(isset($_SESSION['error']['desc']))
+                                    {
+                                        echo '<p class="error">'.$_SESSION['error']['desc'].'</p>';
+                                    }
+                                ?>
+                            </div>
+
+                            <div class="form-floating mb-3">
+                                <input name="price" type="text" class="form-control rouneded-3" placeholder="Price">
+                                <label>Price</label>
+                                <?php
+                                    if(isset($_SESSION['error']['price']))
+                                    {
+                                        echo '<p class="error">'.$_SESSION['error']['price'].'</p>';
+                                    } 
+                                ?>
+                            </div>
+
+                            <div class="form-group">
+                                <?php
+                                    if(isset($_SESSION['error']['b_img']))
+                                    {
+                                        echo '<p class="error">'.$_SESSION['error']['b_img'].'</p>';
+                                    }
+                                ?>
+                                <input type="file" name="b_img" class="form-control">
+                            </div>
+                            <br>
+
+                            <button type="submit" class="btn btn-info btn-sm">Update Book</button>
+
+                            <a href="book_view.php" class="btn btn-info btn-sm">Exit</a>
+
+                        </form>
+
+                        <?php
+                            unset($_SESSION['error']);
+                        ?>
+
                 </div>
             </div>
         </div>
+    </div>
 
 <?php
     include("includes/footer.php");
