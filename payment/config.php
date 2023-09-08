ccb4dd9df78bb6f5f394bcbedb4d60755c145465
We1337 <githubdevice@gmail.com>
2023-09-08 16:03:21 +0600

    feat: paypal payment system

---
+++ payment/config.php
@@ -0,0 +1,24 @@
+<?php
+
+// Database Configuration 
+define('DB_HOST', 'localhost'); 
+define('DB_NAME', 'Your Database Name'); 
+define('DB_USERNAME', 'Your Database Username'); 
+define('DB_PASSWORD', 'Your Database Password'); 
+
+// PayPal Configuration
+define('PAYPAL_EMAIL', 'Your PayPal Business Email'); 
+define('RETURN_URL', 'https://www.your-website.com/return.php'); 
+define('CANCEL_URL', 'https://www.your-website.com/cancel.php'); 
+define('NOTIFY_URL', 'https://www.your-website.com/notify.php'); 
+define('CURRENCY', 'USD'); 
+define('SANDBOX', TRUE); // TRUE or FALSE 
+define('LOCAL_CERTIFICATE', FALSE); // TRUE or FALSE
+
+if (SANDBOX === TRUE){
+	$paypal_url = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+}else{
+	$paypal_url = "https://www.paypal.com/cgi-bin/webscr";
+}
+// PayPal IPN Data Validate URL
+define('PAYPAL_URL', $paypal_url);
