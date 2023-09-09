18eee1a760b29a76a2a5b67aa8af38a8668a473a
We1337 <githubdevice@gmail.com>
2023-09-09 10:57:33 +0600

    feat: cancel page added

---
+++ payment/cancel.php
@@ -0,0 +1,12 @@
+<?php
+    include("includes/header.php");
+?>
+
+        <div class="mh-100" style="height:500px">
+            <br>
+            <h1 class="text-center h-50">Sorry! Your PayPal Payment has been cancelled.</h1>
+        </div>
+ 
+<?php
+    include("includes/footer.php");
+?>
