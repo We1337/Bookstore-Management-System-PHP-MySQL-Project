5e658713b08a98a9aaf1a5f7c8a1bf1b4770e812
We1337 <githubdevice@gmail.com>
2023-09-10 09:31:47 +0600

    feat: payment style updated

--- payment.php
+++ payment.php
@@ -1,43 +1,45 @@
 <?php
 
 require_once('payment/dbclass.php');
+include("includes/header.php");
+
 ?>
 
-<html>
-<head>
-<title>PayPal Payment Integration in PHP</title>
-</head>
-<body>
+        <br>
+        <h2 class="text-center">PayPal Payment Integration in PHP</h2>
+        <br>
+        <div style='text-align: center;'>
+            <div class='product_wrapper'>
+                <h2 class='price'>Total price KZT: <?php echo $_GET['price']; ?></h2>
+                <br>
+                <form method='POST' action='<?php echo PAYPAL_URL; ?>'>
 
-<h2>PayPal Payment Integration in PHP</h2>
+                    <!-- PayPal business email to collect payments -->
+                    <input type='hidden' name='business' value='<?php echo PAYPAL_EMAIL; ?>'>
+
+                    <!-- Details of item that customers will purchase -->
+                    <input type='hidden' name='item_number' value='<?php echo 12; ?>'>
+                    <input type='hidden' name='item_name' value='<?php echo 'books'; ?>'>
+                    <input type='hidden' name='amount' value='<?php echo $_GET['price']; ?>'>
+                    <input type='hidden' name='currency_code' value='<?php echo CURRENCY; ?>'>
+                    <input type='hidden' name='no_shipping' value='1'>
+                    
+                    <!-- PayPal return, cancel & IPN URLs -->
+                    <input type='hidden' name='return' value='<?php echo RETURN_URL; ?>'>
+                    <input type='hidden' name='cancel_return' value='<?php echo CANCEL_URL; ?>'>
+                    <input type='hidden' name='notify_url' value='<?php echo NOTIFY_URL; ?>'>
+
+                    <!-- Specify a Pay Now button. -->
+                    <input type="hidden" name="cmd" value="_xclick">
+                    <button type='submit' class='btn btn-outline-primary'>Pay Now</button>
+                    
+                </form>
+            </div>
+        </div>
 
 
-        <div class='product_wrapper'>
-            <div class='name'><?php echo 'hello'; ?></div>
-            <div class='price'>$<?php echo $_GET['price']; ?></div>
-            <form method='post' action='<?php echo PAYPAL_URL; ?>'>
+<?php
 
-                <!-- PayPal business email to collect payments -->
-                <input type='hidden' name='business' value='<?php echo PAYPAL_EMAIL; ?>'>
+include("includes/footer.php");
 
-                <!-- Details of item that customers will purchase -->
-                <input type='hidden' name='item_number' value='<?php echo 12; ?>'>
-                <input type='hidden' name='item_name' value='<?php echo 'books'; ?>'>
-                <input type='hidden' name='amount' value='<?php echo $_GET['price']; ?>'>
-                <input type='hidden' name='currency_code' value='<?php echo CURRENCY; ?>'>
-                <input type='hidden' name='no_shipping' value='1'>
-                
-                <!-- PayPal return, cancel & IPN URLs -->
-                <input type='hidden' name='return' value='<?php echo RETURN_URL; ?>'>
-                <input type='hidden' name='cancel_return' value='<?php echo CANCEL_URL; ?>'>
-                <input type='hidden' name='notify_url' value='<?php echo NOTIFY_URL; ?>'>
-
-                <!-- Specify a Pay Now button. -->
-                <input type="hidden" name="cmd" value="_xclick">
-                <button type='submit' class='pay'>Pay Now</button>
-                
-            </form>
-		</div>
-
-</body>
-</html>
+?>
