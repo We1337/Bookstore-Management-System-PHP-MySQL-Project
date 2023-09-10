413d052c91da0decc0d15e2ca55c73ceb30f11b6
We1337 <githubdevice@gmail.com>
2023-09-10 09:03:50 +0600

    bug: fixed

--- payment/notify.php
+++ payment/notify.php
@@ -23,7 +23,7 @@ if(function_exists('get_magic_quotes_gpc')) {
 	$get_magic_quotes_exists = true;
 }
 foreach ($myPost as $key => $value) {
-	if($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
+	if($get_magic_quotes_exists == true && $get_magic_quotes_gpc == 1) {
 		$value = urlencode(stripslashes($value));
 	} else {
 		$value = urlencode($value);
