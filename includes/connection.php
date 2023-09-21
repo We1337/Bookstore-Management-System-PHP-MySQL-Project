e211618d99b37b97bf3592a2ef58d84492a440e8
We1337 <githubdevice@gmail.com>
2023-09-21 21:11:35 +0600

    refactor: connection database

--- includes/connection.php
+++ includes/connection.php
@@ -1,14 +1,13 @@
 <?php
-	class ConnectionDataBase {
-		private const HOST = 'localhost';
-		private const USER = 'root';
-		private const PASSWORD = 'DedSec44678@';
-		private const DATABASE = 'bookstoredatabase';
+	define("DB_HOST", "localhost");
+	define("DB_NAME", "bookstoredatabase");
+	define("DB_USER", "root");
+	define("DB_PASS", "DedSec44678@");
 
-		public function connection_database() {
-			
-			$mysqli = new mysqli(HOST, );
-			return $mysqli;
-		}
+	try {
+		$connection_db = new mysqli(DB_HOST, DB_NAME, DB_USER, DB_PASS);
+	} catch(Exception $error_message) {
+		$error = $error_message->getMessage();
+		echo "Error: " . $error;
 	}
 ?>
