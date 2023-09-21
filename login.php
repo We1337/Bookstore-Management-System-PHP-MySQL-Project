83cccd9a52928c110da21c85e5cae84e7c5d8244
We1337 <githubdevice@gmail.com>
2023-09-21 21:29:52 +0600

    refactor: login page variables name repaced

--- login.php
+++ login.php
@@ -1,52 +1,49 @@
 <?php
-// Include the header of the page
-include("includes/header.php");
+    include("includes/header.php");
 ?>
 
-<!-- Login Modal -->
-<div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSignin">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content rounded-4 shadow">
-            <!-- Modal Header -->
-            <div class="modal-header p-5 pb-4 border-bottom-0">
-                <h1 class="fw-bold mb-0 fs-2">Login</h1>
-            </div>
-
-            <!-- Modal Body -->
-            <div class="modal-body p-5 pt-0">
-                <!-- Login Form -->
-                <form class="login" action="login_process.php" method="POST">
-                    <?php
-                    // Display error messages, if any
-                    if (!empty($_SESSION['error'])) {
-                        foreach ($_SESSION['error'] as $er) {
-                            echo '<div class="alert alert-info" role="alert">' . $er . '</div>';
-                        }
-                        unset($_SESSION['error']);
-                    }
-                    ?>
-                    <div class="form-floating mb-3">
-                        <input name="unm" type="text" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com">
-                        <label for="floatingInput">Email address or ID</label>
+        <div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content rounded-4 shadow">
+                    <!-- Modal Header -->
+                    <div class="modal-header p-5 pb-4 border-bottom-0">
+                        <h1 class="fw-bold mb-0 fs-2">Login</h1>
                     </div>
-                    <div class="form-floating mb-3">
-                        <input name="pwd" type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password">
-                        <label for="floatingPassword">Password</label>
-                    </div>
-                    <button class="w-100 mb-2 btn btn-sm rounded-3 btn-primary" type="submit" value="Login">Login</button>
-                    <a href="register.php" class="w-100 mb-2 btn btn-sm rounded-3 btn-primary" role="button">Registration</a>
-                    <a href="forget_password.php" class="w-100 py-2 mb-2 btn btn-outline-primary btn-sm rounded-3" role="button">Forget Password?</a>
 
-                    <!-- Admin Section -->
-                    <h2 class="fs-5 fw-bold mb-3">For Admins</h2>
-                    <a href="admin/index.php" class="w-100 py-2 mb-2 btn btn-outline-primary btn-sm rounded-3" role="button">Admin Login</a>
-                </form>
+                    <!-- Modal Body -->
+                    <div class="modal-body p-5 pt-0">
+                        <!-- Login Form -->
+                        <form class="login" action="login_process.php" method="POST">
+                            <?php
+                                // Display error messages, if any
+                                if (!empty($_SESSION['error'])) {
+                                    foreach ($_SESSION['error'] as $errors) {
+                                        echo '<div class="alert alert-info" role="alert">' . $errors . '</div>';
+                                    }
+                                    unset($_SESSION['error']);
+                                }
+                            ?>
+                            <div class="form-floating mb-3">
+                                <input name="username" type="text" class="form-control rounded-3" placeholder="name@example.com">
+                                <label for="floatingInput">Email address or ID</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input name="password" type="password" class="form-control rounded-3" placeholder="Password">
+                                <label for="floatingPassword">Password</label>
+                            </div>
+                            <button class="w-100 mb-2 btn btn-sm rounded-3 btn-primary" type="submit" value="Login">Login</button>
+                            <a href="register.php" class="w-100 mb-2 btn btn-sm rounded-3 btn-primary" role="button">Registration</a>
+                            <a href="forget_password.php" class="w-100 py-2 mb-2 btn btn-outline-primary btn-sm rounded-3" role="button">Forget Password?</a>
+
+                            <!-- Admin Section -->
+                            <h2 class="fs-5 fw-bold mb-3">For Admins</h2>
+                            <a href="admin/index.php" class="w-100 py-2 mb-2 btn btn-outline-primary btn-sm rounded-3" role="button">Admin Login</a>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
 
 <?php
-// Include the footer of the page
-include("includes/footer.php");
+    include("includes/footer.php");
 ?>
--- login_process.php
+++ login_process.php
@@ -1,38 +1,38 @@
 <?php
-session_start();
+    session_start();
 
-if (!empty($_POST)) {
-    extract($_POST);
-    $_SESSION['error'] = [];
+    if (!empty($_POST)) {
+        extract($_POST);
+        $_SESSION['error'] = [];
 
-    // Check if username or password is empty
-    if (empty($unm) || empty($pwd)) {
-        $_SESSION['error'][] = "Please enter a User Name and Password";
-        header("location: login.php");
-    } else {
-        include("includes/connection.php");
-
-        // Query the database to check for valid credentials
-        $q = "SELECT * FROM register WHERE r_unm='$unm' AND r_pwd='$pwd'";
-        $res = mysqli_query($link, $q);
-        $row = mysqli_fetch_assoc($res);
-
-        if (!empty($row)) {
-            // Store user information in the session
-            $_SESSION['client'] = [
-                'unm' => $row['r_fnm'],
-                'id' => $row['r_id'],
-                'status' => true
-            ];
-            header("location: index.php?message=loginsuccess&username='$unm'");
-        } else {
-            // Invalid username or password
-            $_SESSION['error'][] = "Wrong Username or Password";
+        // Check if username or password is empty
+        if (empty($username) || empty($password)) {
+            $_SESSION['error'][] = "Please enter a User Name and Password";
             header("location: login.php");
+        } else {
+            include("includes/connection.php");
+
+            // Query the database to check for valid credentials
+            $query = "SELECT * FROM register WHERE register_full_name='$username' AND register_password='$password'";
+            $res = mysqli_query($connection_database, $query);
+            $row = mysqli_fetch_assoc($res);
+
+            if (!empty($row)) {
+                // Store user information in the session
+                $_SESSION['client'] = [
+                    'username' => $row['register_full_name'],
+                    'id' => $row['register_id'],
+                    'status' => true
+                ];
+                header("location: index.php?message=loginsuccess&username='$username'");
+            } else {
+                // Invalid username or password
+                $_SESSION['error'][] = "Wrong Username or Password";
+                header("location: login.php");
+            }
         }
+    } else {
+        // No form submission data, redirect to login page
+        header("location: login.php");
     }
-} else {
-    // No form submission data, redirect to login page
-    header("location: login.php");
-}
 ?>
