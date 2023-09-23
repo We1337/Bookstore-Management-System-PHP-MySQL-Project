092b6ecb37f631271f89933708d7b7f4038aa87f
We1337 <githubdevice@gmail.com>
2023-09-23 23:59:05 +0600

    refactor: database table and data

--- database/database.sql
+++ database/database.sql
@@ -22,29 +22,31 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Table structure for table `admin_table`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
+CREATE TABLE IF NOT EXISTS `admin_table` (
   `admin_id` INT(4) NOT NULL AUTO_INCREMENT,
   `admin_user_name` VARCHAR(30) NOT NULL,
   `admin_password` VARCHAR(30) NOT NULL,
   PRIMARY KEY (`admin_id`)
-) ENGINE = InnoDB  DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 2;
+) ENGINE = InnoDB  DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 1;
 
 --
--- Dumping data for table `admin`
+-- Dumping data for table `admin_table`
 --
 
-INSERT INTO `admin` (`admin_user_name`, `admin_password`) VALUES ('admin', 'admin');
+INSERT INTO `admin_table` (`admin_user_name`, `admin_password`) VALUES 
+('admin', 'admin'), 
+('root', 'root');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `book`
+-- Table structure for table `book_table`
 --
 
-CREATE TABLE IF NOT EXISTS `book` (
+CREATE TABLE IF NOT EXISTS `book_table` (
   `book_id` INT(10) NOT NULL AUTO_INCREMENT,
   `book_name` VARCHAR(50) NOT NULL,
   `book_category` VARCHAR(50) NOT NULL,
@@ -53,13 +55,13 @@ CREATE TABLE IF NOT EXISTS `book` (
   `book_img` VARCHAR(50) NOT NULL,
   `book_time` INT(20) NOT NULL,
   PRIMARY KEY (`book_id`)
-) ENGINE = InnoDB  DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 2;
+) ENGINE = InnoDB  DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 1;
 
 --
--- Dumping data for table `book`
+-- Dumping data for table `book_table`
 --
 
-INSERT INTO `book` (`book_name`, `book_category`, `book_description`, `book_price`, `book_img`, `book_time`) VALUES
+INSERT INTO `book_table` (`book_name`, `book_category`, `book_description`, `book_price`, `book_img`, `book_time`) VALUES
 ('Octavia Spencer - Detective', 'Suspence', 'This is Detective Book About Ninja', 50, 'book_img/d.jpg', 1554088592),
 ('Murder on The Orient Express', 'Suspence', 'Suspence Book about Murder.', 60, 'book_img/d3.jpg', 1554088749),
 ('A Dictionary of Architecture', 'Architecture', 'Containing over 5,000 entries from Aalto to ziggurat, this is the most comprehensive and up-to-date dictionary of architecture in paperback. Beautifully illustrated and written in a clear and concise style, it is an invaluable work of reference for both students of architecture and the general reader, as well as professional architects. Covers all periods of Western architectural history, from ancient times to the present day Concise biographies of leading architects, from Brunelleschi and Imhotep to Le Corbusier and Richard Rogers Over 250 illustrations specially drawn for this volume', 500, 'book_img/ARC9.jpg', 1554089362),
@@ -75,20 +77,20 @@ INSERT INTO `book` (`book_name`, `book_category`, `book_description`, `book_pric
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
+-- Table structure for table `category_table`
 --
 
-CREATE TABLE IF NOT EXISTS `category` (
+CREATE TABLE IF NOT EXISTS `category_table` (
   `category_id` INT(10) NOT NULL AUTO_INCREMENT,
   `category_name` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 2;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 1;
 
 --
--- Dumping data for table `category`
+-- Dumping data for table `category_table`
 --
 
-INSERT INTO `category` (`category_name`) VALUES
+INSERT INTO `category_table` (`category_name`) VALUES
 ('Suspence'),
 ('Architecture'),
 ('Competitive Exam'),
@@ -102,89 +104,91 @@ INSERT INTO `category` (`category_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `contact`
+-- Table structure for table `contact_table`
 --
 
-CREATE TABLE IF NOT EXISTS `contact` (
+CREATE TABLE IF NOT EXISTS `contact_table` (
   `contact_id` INT(4) NOT NULL AUTO_INCREMENT,
   `contact_full_name` VARCHAR(100) NOT NULL,
-  `contact_mobile_number` INT(10) NOT NULL,
+  `contact_mobile_number` INT(11) NOT NULL,
   `contact_email` VARCHAR(60) NOT NULL,
   `contact_message` LONGTEXT NOT NULL,
   `contact_time` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`contact_id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 2;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 1;
 
 --
--- Dumping data for table `contact`
+-- Dumping data for table `contact_table`
 --
 
-INSERT INTO `contact` (`contact_full_name`, `contact_mobile_number`, `contact_email`, `contact_message`, `contact_time`) VALUES
-('Kumar Baraiya', 2147483647, 'kumar@gmail.com', 'Awesome Books.', '1554092678'),
-('Dhaval Makwana', 2147483647, 'dhavalmak77@gmail.com', 'Best Books', '1554201509'),
-('Dhaval', 123654789, 'dhaval@gmail.com', 'Looking For New Books', '1554219813');
+INSERT INTO `contact_table` (`contact_full_name`, `contact_mobile_number`, `contact_email`, `contact_message`, `contact_time`) VALUES
+('Alan Batr',    77470000000, 'alanbatr@gmail.com',    'Awesome Books.',         '1554092678'),
+('Mergen Altai', 77470000000, 'mergenaltai@gmail.com', 'Best Books',             '1554201509'),
+('Saq Kipchak',  77470000000, 'saqkipchak@gmail.com',  'Looking For New Books',  '1554219813');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order`
+-- Table structure for table `order_table`
 --
 
-CREATE TABLE IF NOT EXISTS `order` (
+CREATE TABLE IF NOT EXISTS `order_table` (
   `order_id` INT(11) NOT NULL AUTO_INCREMENT,
   `order_name` VARCHAR(30) NOT NULL,
   `order_address` VARCHAR(200) NOT NULL,
   `order_pincode` INT(20) NOT NULL,
   `order_city` VARCHAR(30) NOT NULL,
   `order_state` VARCHAR(30) NOT NULL,
-  `order_mobile` VARCHAR(11) NOT NULL,
+  `order_mobile` INT(11) NOT NULL,
   `order_register_id` INT(8) NOT NULL,
   `order_total_price` INT(50) NOT NULL,
   `order_list_books` LONGTEXT NOT NULL,
   PRIMARY KEY (`order_id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 2;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 1;
 
 --
--- Dumping data for table `order`
+-- Dumping data for table `order_table`
 --
 
-INSERT INTO `order` (`order_name`, `order_address`, `order_pincode`, `order_city`, `order_state`, `order_mobile`, `order_rid`, `order_total_price`, `order_list_books`) VALUES
-('Dhaval Akal', 'Mahuva', 125478, 'nuihu', 'Gujarat', 123456789, 1, 500000, 'book_id-amout: 15-12, book_id-amout: 16-10'),
-('Dhaval Makwana', 'Mahuva', 125478, 'nuihu', 'Gujarat', 123456789, 1, 500000, 'book_id-amout: 15-12, book_id-amout: 16-10');
+INSERT INTO `order_table` (`order_name`, `order_address`, `order_pincode`, `order_city`, `order_state`, `order_mobile`, `order_rid`, `order_total_price`, `order_list_books`) VALUES
+('Ulan Batr', 'St. Astana 51', 000000, 'Akmola', 'Astana', 77470000000, 1, 500000, 'book_id-amout: 15-12, book_id-amout: 16-10'),
+('Mete Khan', 'St. Astana 21', 000000, 'Akmola', 'Astana', 77470000000, 1, 540000, 'book_id-amout: 15-12, book_id-amout: 16-10');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `register`
+-- Table structure for table `register_table`
 --
 
-CREATE TABLE IF NOT EXISTS `register` (
+CREATE TABLE IF NOT EXISTS `register_table` (
   `register_id` INT(8) NOT NULL AUTO_INCREMENT,
   `register_full_name` VARCHAR(100) NOT NULL,
   `register_user_name` VARCHAR(50) NOT NULL,
   `register_password` VARCHAR(30) NOT NULL,
-  `register_contact_number` VARCHAR(11) NOT NULL,
+  `register_contact_number` INT(11) NOT NULL,
   `register_email` VARCHAR(60) NOT NULL,
   `register_question` VARCHAR(100) NOT NULL,
   `register_answer` VARCHAR(50) NOT NULL,
   `register_time` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`register_id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 2;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 1;
 
 --
--- Dumping data for table `register`
+-- Dumping data for table `register_table`
 --
 
-INSERT INTO `register` (`register_full_name`, `register_user_name`, `register_password`, `register_contact_number`, `register_email`, `register_question`, `register_answer`, `register_time`) VALUES
-('Dhaval Makwana', 'dhaval', 'dhaval123', '9876543210', 'dhavalmak77@gmail.com', 'Which is your Favourite Movie ?', 'shawshank redemption', '1554092678'),
-('Kumar Baraiya', 'kumar', 'kumar123', '1234567890', 'kbaraiya@gmail.com', 'Which is your Favourite Movie ?', '123', '1554201585'),
-('Hiren Makwana', 'hirenmakwana', 'hirenmakwana', '7894561230', 'hiren@gmail.com', 'Which is your Favourite Movie ?', 'prometheus', '1555655074');
+INSERT INTO `register_table` (`register_full_name`, `register_user_name`, `register_password`, `register_contact_number`, `register_email`, `register_question`, `register_answer`, `register_time`) VALUES
+('Asl Khan',    'Aslkhan', 'asl123',      77470000000, 'aslturkkhan77@gmail.com', 'Which is your Favourite Movie ?', 'redemption', '1554092678'),
+('Tatar Batr',  'Tatar',   'tatar12345',  77470000000, 'tatarbatr@gmail.com',     'Which is your Favourite Movie ?', '123',        '1554201585'),
+('Xasak Ata',   'Ata',     'ataxasak000', 77470000000, 'xasakata@gmail.com',      'Which is your Favourite Movie ?', 'prometheus', '1555655074');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `userSupport`
+-- Table structure for table `user_support_table`
 --
 
-CREATE TABLE IF NOT EXISTS `user_support` (
+CREATE TABLE IF NOT EXISTS `user_support_table` (
     `user_support_id` INT(8) NOT NULL AUTO_INCREMENT,
     `user_support_email` VARCHAR(255) NOT NULL,
     `user_support_subject` VARCHAR(255) NOT NULL,
@@ -192,5 +196,13 @@ CREATE TABLE IF NOT EXISTS `user_support` (
     `user_support_status` ENUM('Open', 'Closed') DEFAULT 'Open',
     `user_support_created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`user_support_id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 2;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 AUTO_INCREMENT = 1;
 
+--
+-- Dumping data for table `user_support_table`
+-- 
+
+INSERT INTO `user_support_table` (`user_support_id`, `user_support_email`, `user_support_subject`, `user_support_message`) VALUES
+('turkkhan@gmail.com', 'User error',          'Help with with error login system'),
+('tatar@gmail.com',    'Delivery problem',    'Hi there, help me with delivery'),
+('aslkhan@gmail.com',  'How to search books', 'Hello, I can not find books');
