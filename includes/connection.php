790250388249353660289cafc31bb8bf4d36cb82
We1337 <githubdevice@gmail.com>
2023-09-24 17:17:39 +0600

    refactor: connection to database updated

--- includes/connection.php
+++ includes/connection.php
@@ -5,9 +5,13 @@
 	define("DB_PASS", "DedSec44678@");
 
 	try {
-		$connection_database = new mysqli(DB_HOST, DB_NAME, DB_USER, DB_PASS);
+		// Connection to database
+		$connection_database = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
 	} catch(Exception $error) {
+		// Error message
 		$error_message = $error->getMessage();
 		echo "Failed to connect to MySQL: " . $error_message;
+
 	}
 ?>
