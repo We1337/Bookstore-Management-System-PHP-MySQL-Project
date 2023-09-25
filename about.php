4d333af45fd7d1da5b4cd00895369f036d0a1154
We1337 <githubdevice@gmail.com>
2023-09-25 21:48:50 +0600

    refactor: dummy text added with comments

--- about.php
+++ about.php
@@ -4,13 +4,65 @@
 
     <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
         <div class="py-5 text-center">
+
+            <!-- Logo image -->
             <img class="d-block mx-auto mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-            <h2>Checkout form</h2>
-            <!-- Placeholder text -->
-            <p class="lead">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                <!-- More text here -->
+            <br>
+
+            <!-- Dummy data -->
+            <h2>About Us</h2> 
+            <p class="text-justify container">
+                Welcome to Bookstore, your premier destination for literature and knowledge. We are passionate about books and dedicated to providing a haven for bibliophiles of all kinds.
             </p>
+            
+            <h4>Our Story</h4>
+            <p class="text-justify container">
+                Founded in 2023, Bookstore has been a pillar of the literary community for over 1 years. It all began when We1337, a lifelong lover of books, decided to turn their passion into a profession. With a small collection of books and a big dream, We1337 opened the doors of our beloved bookstore.
+                <br>
+                Since then, we have grown into a thriving hub for book enthusiasts, offering a vast selection of genres and a cozy atmosphere that encourages exploration and discovery.
+            </p>
+
+            <h4>Our Mission</h4>
+            <p class="text-justify container">
+                At Bookstore, our mission is simple: to ignite the love for reading in every person who walks through our doors. We believe in the power of stories to educate, inspire, and connect people. We are committed to:
+                <br><br>
+                Providing a diverse and carefully curated collection of books.
+                <br>
+                Creating a welcoming and inclusive space for readers of all backgrounds.
+                <br>
+                Hosting events, book clubs, and author signings to foster a sense of community.
+                <br>
+                Supporting local authors and artists by showcasing their work.
+                <br>
+                Encouraging literacy and a lifelong love of reading.
+            </p>
+
+            <h4>Meet Our Team</h4>
+            <p class="text-justify container">
+                Our dedicated team of bookworms is here to assist you on your literary journey. We are a group of individuals who share a deep passion for books and a commitment to serving our community. Get to know us:
+                <br><br>
+                We1337: The visionary behind [Bookstore Name] and a book connoisseur.
+                <br>
+                Same: Our store manager, who ensures everything runs smoothly.
+                <br>
+                Bob: The fiction aficionado who can recommend your next great read.
+                <br>
+                Samanda: The non-fiction guru with a wealth of knowledge.
+                <br>
+                Frank: The mastermind behind our engaging events and book clubs.
+                <br>
+                Rodger: The friendly faces you'll see around the store, ready to assist you.
+            </p>
+            
+            <h4>Our Store</h4>
+            <p class="text-justify container">
+                Located in the heart of Rudny, our bookstore boasts a warm and inviting ambiance. From comfy reading nooks to shelves stacked with literary treasures, every corner of our store is designed to inspire. Come and explore our carefully curated selection, find your next adventure between the pages, and immerse yourself in the world of words.
+                <br>
+                Thank you for being a part of the RII family. We look forward to sharing the joy of reading with you for many more years to come.
+                <br>
+                Please replace the placeholders (in brackets) with specific details for your fictional bookstore.
+            </p>
+
         </div>
     </div>
 
