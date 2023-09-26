cf23c8452548b0bd246e7ed1ed9efd902a957e5e
We1337 <githubdevice@gmail.com>
2023-09-26 09:27:07 +0600

    refactor: message changed geting data from url

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
+
     include("includes/header.php");
 
     // Display messages based on the 'message' query parameter
     $messages = [
         'success' => '<div class="alert alert-success" role="alert">Message has been delivered.</div>',
         'error' => '<div class="alert alert-danger" role="alert">Please try again later.</div>',
-        'loginsuccess' => '<div class="alert alert-success" role="alert">Welcome! Successfully logged in.</div>',
+        'welcome' => '<div class="alert alert-success" role="alert">Welcome! Successfully logged in.</div>',
     ];
 
     $messageType = $_GET['message'] ?? '';
