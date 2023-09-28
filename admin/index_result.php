7736c90be14682e48a82fa88c77025070ee77c9b
We1337 <githubdevice@gmail.com>
2023-09-28 22:32:55 +0600

    refactor: connection to new database

--- admin/index_result.php
+++ admin/index_result.php
@@ -1,20 +1,20 @@
 <?php
 include("../includes/connection.php");
 
-$countUsers = "SELECT count(*) AS total FROM `register`;";
-$resUsers = mysqli_query($link, $countUsers);
+$countUsers = "SELECT count(*) AS total FROM `register_table`;";
+$resUsers = mysqli_query($connection_database, $countUsers);
 $realResultUsers =  mysqli_fetch_assoc($resUsers);
    
-$countBooks = "SELECT count(*) AS total FROM `book`;";
-$resBooks = mysqli_query($link, $countBooks);
+$countBooks = "SELECT count(*) AS total FROM `book_table`;";
+$resBooks = mysqli_query($connection_database, $countBooks);
 $realResultBooks = mysqli_fetch_assoc($resBooks);
 
-$countOrder = "SELECT count(*) AS total FROM `order`;";
-$resOrder = mysqli_query($link, $countOrder);
+$countOrder = "SELECT count(*) AS total FROM `order_table`;";
+$resOrder = mysqli_query($connection_database, $countOrder);
 $realResultOrder = mysqli_fetch_assoc($resOrder);
 
-$countContact = "SELECT count(*) AS total FROM `contact`;";
-$resContact = mysqli_query($link, $countContact);
+$countContact = "SELECT count(*) AS total FROM `contact_table`;";
+$resContact = mysqli_query($connection_database, $countContact);
 $realResultContact = mysqli_fetch_assoc($resContact);
 
 ?>
