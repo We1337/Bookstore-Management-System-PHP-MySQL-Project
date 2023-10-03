e01b1d346eeedfbb67f2006025cf189a4024e31a
We1337 <githubdevice@gmail.com>
2023-10-03 21:58:13 +0600

    fix: bugs fixed for profile update data

--- functions/profile_process.php
+++ functions/profile_process.php
@@ -20,43 +20,51 @@
         // Validate Full Name
         if (empty($fullname)) 
         {
-            $_SESSION['error']['fullname'] = "Please enter Full Name";
+            $_SESSION['error']['fullname'] = "Please enter full name";
         }
 
         // Validate User Name
         if (empty($username)) 
         {
-            $_SESSION['error']['username'] = "Please enter User Name";
+            $_SESSION['error']['username'] = "Please enter user ID";
+        } 
+        else if (strpos($username, ' ') !== false) 
+        {
+            $_SESSION['error']['username'] = "Please enter user without space";
         }
 
         // Validate Password
         if (empty($password)) 
         {
-            $_SESSION['error']['password'] = "Please enter Password";
+            $_SESSION['error']['password'] = "Please enter password";
         } 
         else if (strlen($password) < 8) 
         {
-            $_SESSION['error']['password'] = "Please Enter Minimum 8 Digit Password";
+            $_SESSION['error']['password'] = "Please enter minimum 8 digit password";
+        }
+        else if (strpos($password, ' ') !== false) 
+        {
+            $_SESSION['error']['password'] = "Please enter password without space";
         }
 
         // Validate E-Mail Address
         if (empty($email)) 
         {
-            $_SESSION['error']['email'] = "Please enter E-Mail Address";
+            $_SESSION['error']['email'] = "Please enter E-Mail address";
         } 
         else if (!preg_match("/^[a-z0-9]+@[a-z\.]+$/i", $email)) 
         {
-            $_SESSION['error']['email'] = "Please Enter Valid E-Mail Address";
+            $_SESSION['error']['email'] = "Please enter valid E-Mail address";
         }
 
         // Validate Contact Number
         if (empty($contact)) 
         {
-            $_SESSION['error']['contact_number'] = "Please Enter Contact Number";
+            $_SESSION['error']['contact_number'] = "Please enter contact number";
         } 
-        elseif (!is_numeric($contact)) 
+        else if (!is_numeric($contact)) 
         {
-            $_SESSION['error']['contact_number'] = "Please Enter Contact Number in Numbers";
+            $_SESSION['error']['contact_number'] = "Please enter contact number in digits";
         }
 
         if (!empty($rows['register_profile_picture']))
@@ -66,9 +74,19 @@
 
 		if (!empty($_FILES['file']['name'])) 
 		{
-			move_uploaded_file($_FILES['file']['tmp_name'], "../profile_img/" . $_FILES['file']['name']);
-			$img = "profile_img/" . $_FILES['file']['name'];
-		}	
+            $allowedExtensions = ['png', 'jpg', 'jpeg'];
+            $fileExtension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+
+            if(!in_array(strtolower($fileExtension), $allowedExtensions)) 
+            {
+			    $_SESSION['error']['file'] = "Please upload a PNG or JPG image";
+            } 
+            else 
+            {
+                move_uploaded_file($_FILES['file']['tmp_name'], "../profile_img/" . $_FILES['file']['name']);
+			    $img = "profile_img/" . $_FILES['file']['name'];
+            }
+		}
 
         // If there are validation errors, redirect back to the registration page
         if (!empty($_SESSION['error'])) 
@@ -82,7 +100,7 @@
         
             mysqli_query($connection_database, $query);
 
-            header("location: ../profile.php?message=saved");
+            header("location: ../profile.php");
         }
     } 
     else 
--- profile.php
+++ profile.php
@@ -2,9 +2,9 @@
     include("includes/header.php");
     include("includes/connection.php");
     include("functions/profile_data.php");
+    include("functions/profile_notification.php");
 
-    // Create a message for user saved a file.
-  
+    display_notification_messages(); 
 ?>
 
         <div class="container-xl px-4 mt-4">
@@ -13,54 +13,55 @@
                     <form action="functions/profile_process.php" method="POST" enctype="multipart/form-data">
                         <div class="card mb-4 mb-xl-0">
 
-                                <div class="card-header">Profile Picture</div>
-                                    <div class="card-body text-center">
+                            <div class="card-header">Profile Picture</div>
+                                <div class="card-body text-center">
 
-                                        <img class="img-account-profile rounded-circle mb-2" style="width:220px;height:220px;" src="<?php echo $row['register_profile_picture'] ?>" alt>
+                                    <img class="img-account-profile rounded-circle mb-2" style="width:220px;height:220px;" src="<?php echo $row['register_profile_picture'] ?>" alt>
 
-                                        <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
+                                    <div class="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
 
-                                        <input name="file" type="file" class="form-control">
+                                    <input name="file" type="file" class="form-control">
 
-                                    </div>
                                 </div>
+                            </div>
+
                         </div>
                         <div class="col-xl-8">
                             <div class="card mb-4">
                                 <div class="card-header">Account Details</div>
                                     <div class="card-body">
                                         
-                                            <div class="mb-3">
-                                                <label class="small mb-1" for="inputUsername">Username (how your name will appear to other users on the site)</label>
-                                                <input name="username" class="form-control" type="text" placeholder="Enter your username" value="<?php echo $row['register_user_name']; ?>">
-                                            </div>
+                                        <div class="mb-3">
+                                            <label class="small mb-1" for="inputUsername">Username (how your name will appear to other users on the site)</label>
+                                            <input name="username" class="form-control" type="text" placeholder="Enter your username" value="<?php echo $row['register_user_name']; ?>">
+                                        </div>
 
-                                            <div class="row gx-3 mb-3">
-                                                <div class="col-md-6">
-                                                    <label class="small mb-1" for="inputFullName">Full name</label>
-                                                    <input name="fullname" class="form-control" type="text" placeholder="Enter your full name" value="<?php echo $row['register_full_name']; ?>">
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <label class="small mb-1" for="inputPassword">Password</label>
-                                                    <input name="password" class="form-control" type="text" placeholder="Enter your Password" value="<?php echo $row['register_password']; ?>">
-                                                </div>
+                                        <div class="row gx-3 mb-3">
+                                            <div class="col-md-6">
+                                                <label class="small mb-1" for="inputFullName">Full name</label>
+                                                <input name="fullname" class="form-control" type="text" placeholder="Enter your full name" value="<?php echo $row['register_full_name']; ?>">
+                                            </div>
+                                            <div class="col-md-6">
+                                                <label class="small mb-1" for="inputPassword">Password</label>
+                                                <input name="password" class="form-control" type="text" placeholder="Enter your Password" value="<?php echo $row['register_password']; ?>">
+                                            </div>
+                                        </div>
+
+                                        <div class="row gx-3 mb-3">
+                                            <div class="mb-3">
+                                                <label class="small mb-1" for="inputEmailAddress">Email address</label>
+                                                <input name="email" class="form-control" type="email" placeholder="Enter your email address" value="<?php echo $row['register_email']; ?>">
                                             </div>
 
                                             <div class="row gx-3 mb-3">
                                                 <div class="mb-3">
-                                                    <label class="small mb-1" for="inputEmailAddress">Email address</label>
-                                                    <input name="email" class="form-control" type="email" placeholder="Enter your email address" value="<?php echo $row['register_email']; ?>">
+                                                    <label class="small mb-1" for="inputPhone">Phone number</label>
+                                                    <input name="contact" class="form-control" type="tel" placeholder="Enter your phone number" value="<?php echo $row['register_contact_number']; ?>">
                                                 </div>
-
-                                                <div class="row gx-3 mb-3">
-                                                    <div class="col-md-6">
-                                                        <label class="small mb-1" for="inputPhone">Phone number</label>
-                                                        <input name="contact" class="form-control" type="tel" placeholder="Enter your phone number" value="<?php echo $row['register_contact_number']; ?>">
-                                                    </div>
-                                                </div>
-
-                                                <button class="btn btn-primary" type="submit" value="submit">Save changes</button>
                                             </div>
+
+                                            <button class="btn btn-outline-info" type="submit" value="submit">Save changes</button>
+                                        </div>
                                     
                                     </div>
                                 </div>
@@ -70,6 +71,14 @@
                 </div>
             </div>
         </div>
+
+        <!-- JavaScript Code (to Show Modal on Page Load) -->
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                var myModal = new bootstrap.Modal(document.getElementById("myModal"));
+                myModal.show();
+            });
+        </script>
 
 <?php
     include("includes/footer.php");
