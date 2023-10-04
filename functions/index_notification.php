44ebba4bbabd12c81a5b8832be880bcc0ea688a8
We1337 <githubdevice@gmail.com>
2023-10-04 08:17:02 +0600

    feater: notification or messages for user

---
+++ functions/index_notification.php
@@ -0,0 +1,62 @@
+<?php
+
+    function display_notification_messages() {
+        // Display error messages, if any
+        if (!empty($_SESSION['error'])) 
+        {
+            $arrays_of_message = array();
+            foreach ($_SESSION['error'] as $errors) 
+            { 
+                array_push($arrays_of_message, $errors); 
+            }
+
+            $messages = implode("<br> ", $arrays_of_message);
+
+            echo '
+                <!-- Modal HTML -->
+                <div id="myModal" class="modal fade" tabindex="-1">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Warning!</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p class="alert alert-warning">' . $messages . '</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ';
+
+            unset($_SESSION['error']);
+        }
+    }
+
+    function display_notification_messages_sucesses() {
+        // Display error messages, if any
+        if (!empty($_SESSION['message'])) 
+        {
+
+            echo '
+                <!-- Modal HTML -->
+                <div id="myModal" class="modal fade" tabindex="-1">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Welcome!</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p class="alert alert-success">User: ' . $_SESSION['client']['username'] . '</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ';
+
+            unset($_SESSION['message']);
+        }
+    }
+
+?>
--- functions/index_process.php
+++ functions/index_process.php
@@ -2,21 +2,4 @@
     $query = "SELECT * FROM `book_table` ORDER BY `book_id` DESC LIMIT 0,9";
     
     $result = mysqli_query($connection_database, $query);
-
-    function display_notification_messages() {
-        // Display messages based on the 'message' query parameter
-        $messages = [
-            'success' => '<div class="alert alert-success" role="alert">Message has been delivered.</div>',
-            'error' => '<div class="alert alert-danger" role="alert">Please try again later.</div>',
-            'welcome' => '<div class="alert alert-success" role="alert">Welcome! Successfully logged in.</div>',
-            'support_sended' => '<div class="alert alert-success" role="alert">Your message sended to support group.</div>'
-        ];
-
-        $messageType = $_GET['message'] ?? '';
-
-        if (isset($messages[$messageType])) 
-        {
-            echo $messages[$messageType];
-        }
-    }
 ?>
