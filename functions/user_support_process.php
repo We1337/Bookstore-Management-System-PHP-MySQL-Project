67998b614759980b90aaa707af7b2d0d5df158e2
We1337 <githubdevice@gmail.com>
2023-10-05 08:49:34 +0600

    feat: user support send with user id to database

--- functions/user_support_process.php
+++ functions/user_support_process.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+
     include('../includes/connection.php');
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") 
@@ -6,11 +8,13 @@
         $useremail = $_POST["useremail"];
         $subject = $_POST["subject"];
         $message = $_POST["message"];
+        $user_id_support =  intval($_SESSION['client']['id']);
 
-        $query = "INSERT INTO `user_support_table`(`user_support_email`, `user_support_subject`, `user_support_message`) VALUES ('$useremail', '$subject', '$message')";
+        $query = "INSERT INTO `user_support_table`(`user_support_actual_id`, `user_support_email`, `user_support_subject`, `user_support_message`) VALUES ('$user_id_support', '$useremail', '$subject', '$message')";
         
         mysqli_query($connection_database, $query);
-        header("Location: ../index.php?message=support_sended");
+        $_SESSION['message']['success'] = 'Message sended to support group';
+        header("Location: ../index.php");
         exit();
     }
 ?>
