fad08f0b4c6984f2cb1f0eef856f097df79138bf
We1337 <githubdevice@gmail.com>
2023-10-06 08:22:37 +0600

    feat: if connection lost send to user 500 error

---
+++ 500.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+	
+		<title>Bookstore Management System</title>
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width", initial-scale=1.0>
+		<link rel="icon" href="https://getbootstrap.com/docs/5.3/assets/img/favicons/favicon.ico" type="image/x-icon">
+
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+	</head>
+	<body>
+
+        <h1 class="visually-hidden">Heroes examples</h1>
+        <br><br><br><br>
+        <div class="px-4 py-5 my-5 text-center">
+            <img class="d-block mx-auto mb-4" src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+            <h1 class="display-5 fw-bold">Error: 500 <?php echo $connection; ?></h1>
+            <div class="col-lg-6 mx-auto">
+                <p class="lead mb-4">Error code 500, come back leter.</p>
+            </div>
+        </div>
+
+    </body>
+
+</html>
--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,6 @@
 <?php
+	session_start();
+
 	define("DB_HOST", "localhost");
 	define("DB_NAME", "bookstoredatabase");
 	define("DB_USER", "root");
@@ -6,15 +8,13 @@
 
 	try 
 	{
+
 		// Connection to database
 		$connection_database = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
+		
 	}
 	catch(Exception $error) 
 	{
-		// Error message
-		$error_message = $error->getMessage();
-		echo "Failed to connect to MySQL: " . $error_message;
-
+		header("location: ../500.php");
 	}
 ?>
