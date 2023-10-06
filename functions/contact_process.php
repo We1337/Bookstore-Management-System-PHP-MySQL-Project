7c143d2baa4a7c75ba6d8cba48924fd82caa64b7
We1337 <githubdevice@gmail.com>
2023-10-06 07:11:57 +0600

    refactor: session added, index array removed, inserting id, display message for use, id add to database.

--- functions/contact_process.php
+++ functions/contact_process.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
 
     // Check if the form has been submitted
     if (!empty($_POST)) 
@@ -12,40 +13,39 @@
         // Validate Full Name
         if (empty($full_name)) 
         {
-            $_SESSION['error']['full_name'] = "Please enter Full Name";
+            $_SESSION['error'][] = "Please enter full name";
         }
 
         // Validate Mobile Number
         if (empty($mobile_number)) 
         {
-            $_SESSION['error']['mobile_number'] = "Please enter Mobile Number";
+            $_SESSION['error'][] = "Please enter mobile number";
         } 
-        elseif (!empty($mobile_number)) 
-        {
-            // Check if Mobile Number is numeric
+        else if (!empty($mobile_number))
+        { 
             if (!is_numeric($mobile_number)) 
             {
-                $_SESSION['error']['mobile_number'] = "Please Enter Numeric Mobile Number";
+                $_SESSION['error'][] = "Please enter mobile number in digits";
             }
         }
 
         // Validate Message
         if (empty($message)) 
         {
-            $_SESSION['error']['message'] = "Please enter Message";
+            $_SESSION['error'][] = "Please enter message";
         }	
 
         // Validate E-Mail Address
         if (empty($email)) 
         {
-            $_SESSION['error']['email'] = "Please enter E-Mail ID";
+            $_SESSION['error'][] = "Please enter email";
         }
 
         // Check if there are any errors
-        if (!empty($error)) 
+        if (!empty($_SESSION['error'])) 
         {
             // Redirect to the contact page with an error message
-            header("Location: contact.php?message=error");
+            header("Location: ../contact.php");
             exit();
         } 
         else 
@@ -55,21 +55,23 @@
 
             // Get the current timestamp
             $time = time();
+            $user_actual_id = (int)$_SESSION['client']['id'];
 
             // Insert contact form data into the database
-            $query = "INSERT INTO `contact_table`(`contact_full_name`, `contact_mobile_number`, `contact_email`, `contact_message`, `contact_time`) VALUES ('$full_name', '$mobile_number', '$email', '$message', '$time')";
+            $query = "INSERT INTO `contact_table`(`contact_full_name`, `contact_actual_id`, `contact_mobile_number`, `contact_email`, `contact_message`, `contact_time`) VALUES ('$full_name', $user_actual_id, '$mobile_number', '$email', '$message', '$time')";
 
             mysqli_query($connection_database, $query);
 
             // Redirect to the index page with a success message
-            header("Location: ../index.php?message=success");
+            $_SESSION['message']['success'] = "Message sended for contact";
+            header("Location: ../index.php");
             exit();
         }
     } 
     else 
     {
         // If the form was not submitted, redirect to the contact page with an error message
-        header("Location: contact.php?message=error");
+        header("Location: ../contact.php");
         exit();
     }
 ?>
