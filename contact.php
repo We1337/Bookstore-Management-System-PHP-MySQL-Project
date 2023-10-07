f6008c5833b28519737fac4e33e2cfb94c367691
We1337 <githubdevice@gmail.com>
2023-10-07 15:33:52 +0600

    refactor: space given

--- contact.php
+++ contact.php
@@ -10,6 +10,7 @@
         <div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
                 <div class="modal-content rounded-4 shadow">
+                    
                     <div class="modal-header p-5 pb-4 border-bottom-0">
                         <h1 class="fw-bold mb-0 fs-2">Contact Us</h1>
                     </div>
