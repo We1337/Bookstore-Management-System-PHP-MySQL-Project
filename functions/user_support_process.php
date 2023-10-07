9509358af0cd31796b846c3ed536920680f12d38
We1337 <githubdevice@gmail.com>
2023-10-07 15:32:52 +0600

    refactor: user support to check user fill up the form

--- functions/user_support_process.php
+++ functions/user_support_process.php
@@ -1,20 +1,54 @@
 <?php
+
     session_start();
-
-    include('../includes/connection.php');
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") 
+    
+    if (!empty($_POST)) 
     {
-        $useremail = $_POST["useremail"];
-        $subject = $_POST["subject"];
-        $message = $_POST["message"];
-        $user_id_support =  intval($_SESSION['client']['id']);
+        // Extract form data into variables
+        extract($_POST);
 
-        $query = "INSERT INTO `user_support_table`(`user_support_actual_id`, `user_support_email`, `user_support_subject`, `user_support_message`) VALUES ('$user_id_support', '$useremail', '$subject', '$message')";
+        // Initialize an array to store error messages
+        $_SESSION['error'] = array();
+
+        if (empty($user_email))
+        {
+            $_SESSION['error'][] = "Please enter E-Mail address";
+        }
+
+        if (empty($subject))
+        {
+            $_SESSION['error'][] = "Please enter subject";
+        }
+
+        if (empty($message))
+        {
+            $_SESSION['error'][] = "Please enter message";
+        }
+
+        if (!empty($_SESSION['error'])) 
+        {
+            header("location: ../user_support.php");
+            exit();
+        } 
+        else 
+        {
+            include('../includes/connection.php');
+
+            //$time = time();
+            $user_id_support = (int)$_SESSION['client']['id'];
+
+            $query = "INSERT INTO `user_support_table`(`user_support_actual_id`, `user_support_email`, `user_support_subject`, `user_support_message`) VALUES ('$user_id_support', '$useremail', '$subject', '$message')";
         
-        mysqli_query($connection_database, $query);
-        $_SESSION['message']['success'] = 'Message sended to support group';
-        header("Location: ../index.php");
+            mysqli_query($connection_database, $query);
+
+            $_SESSION['message']['success'] = "Message sended to support group";
+            header("location: ../index.php");
+            exit();
+        }
+    }
+    else 
+    {
+        header("location: ../user_support.php");
         exit();
     }
 ?>
