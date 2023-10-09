7f512ef3c25ba2264ba99d7628590260745a02a1
We1337 <githubdevice@gmail.com>
2023-10-09 20:57:18 +0600

    refactor: order page changed for new layout

--- admin/order_view.php
+++ admin/order_view.php
@@ -2,69 +2,64 @@
     include("includes/header.php");
     include("../includes/connection.php");
 ?>
-        <div class="container">
-            <div class="post">
-                <br>
-                <h2 class="title text-center">View Order</h2>
+        <main class="px-md-4">
+			<h2>Order</h2>
 
-                <form method="GET" action="order_view.php" class="form-inline">
-                    <input class="form-control mr-sm-2" type="text" name="s" placeholder="Search" aria-label="Search">
-                    <br>
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="search-submit">Search</button>
-                </form> 
+			<form method="GET" action="../admin/order_view.php" class="form-inline">
+                <input class="form-control mr-sm-2" type="text" name="search" placeholder="Search" aria-label="Search">
                 <br>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="search-submit">Search</button>
+            </form> 
 
-                <table class="table table-striped table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">ID</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Address</th>
-                            <th scope="col">Pincode</th>
-                            <th scope="col">City</th>
-                            <th scope="col">State</th>
-                            <th scope="col">Mobile</th>
-                            <th scope="col">Rid</th>
-                            <th scope="col">Price</th>
+			<div class="table-responsive">
+				<table class="table table-striped table-sm">
+					<thead>
+						<tr>
+							<th scope="col">ID</th>
+							<th scope="col">User order</th>
+							<th scope="col">Address</th>
+							<th scope="col">Postcode</th>
+							<th scope="col">City</th>
+							<th scope="col">State</th>
+							<th scope="col">Phone</th>
+							<th scope="col">User ID</th>
+                            <th scope="col">Total price</th>
                             <th scope="col">Order</th>
-                            <th scope="col">Delate</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                            <th scope="col">Edit</th>
+                            <th scope="col">Delete</th>
+						</tr>
+					</thead>
 
-                        <?php
-                            $s = $_GET['s'];
-                            $blq = "SELECT * FROM `order_table` WHERE `order_name` LIKE '%$s%'";
-                            $blres = mysqli_query($connection_database, $blq);
+					<?php
+						
+						$search = $_GET['search'];
+						$order_list_query = "SELECT * FROM `order_table` WHERE `order_name` LIKE '%$search%'";
+						$order_list_result = mysqli_query($connection_database, $order_list_query);
 
-                            $count = 1;
+						while($order_row = mysqli_fetch_assoc($order_list_result))
+						{
+							echo '
+								<tr>
+									<td>' . $order_row['order_id'] . '</td>
+									<td>' . $order_row['order_name'] . '</td>
+									<td>' . $order_row['order_address'] . '</td>
+									<td>' . $order_row['order_pincode'] . '</td>
+									<td>' . $order_row['order_city'] . '</td>
+                                    <td>' . $order_row['order_state'] . '</td>
+                                    <td>' . $order_row['order_mobile'] . '</td>
+                                    <td>' . $order_row['order_register_id'] . '</td>
+                                    <td>' . $order_row['order_total_price'] . '</td>
+                                    <td>' . $order_row['order_list_books'] . '</td>
+                                    <td align="center"><a class="btn btn-danger btn-sm" href="functions/process_order_del.php?id=' . $order_row['order_id'] . '">Delate</a></td>
+									<td align="center"><a class="btn btn-info btn-sm" href="order_edit.php?id=' . $order_row['order_id'] . '">Edit</a></td>
+								</tr>
+                            ';	
+						}
+					?> 
 
-                            while($r_row = mysqli_fetch_assoc($blres))
-                            {
-                                echo '
-                                    <tr>
-                                        <td>'.$count.'</td>
-                                        <td>'.$r_row['order_id'].'</td>
-                                        <td>'.$r_row['order_name'].'</td>
-                                        <td>'.$r_row['order_address'].'</td>
-                                        <td>'.$r_row['order_pincode'].'</td>
-                                        <td>'.$r_row['order_city'].'</td>
-                                        <td>'.$r_row['order_state'].'</td>
-                                        <td>'.$r_row['order_mobile'].'</td>
-                                        <td>'.$r_row['order_rid'].'</td>
-                                        <td>'.$r_row['order_total_price'].'</td>
-                                        <td>'.$r_row['order_list_books'].'</td>
-                                        <td align="center"><a style="color: red;" href="process_order_del.php?id='.$r_row['order_id'].'">x</a></td>
-                                    </tr>';
-                                $count++;
-                            }
-                        ?>
-                                            
-                    </tbody>
-                </table>
-            </div> 
-        </div>
+				</table>
+			</div>
+		</main>
 
 <?php
     include("includes/footer.php");
