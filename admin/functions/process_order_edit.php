3d8e6c3b21d90de210b8afc90a3266ff3f7be035
We1337 <githubdevice@gmail.com>
2023-10-09 21:04:27 +0600

    refactor: unused query

--- admin/functions/process_order_edit.php
+++ admin/functions/process_order_edit.php
@@ -8,10 +8,6 @@
 		$_SESSION['error'] = array();
 
 		extract($_POST);
-
-		$querys = "SELECT * FROM `order_table` WHERE `order_id` = '$order_id'";
-		$get_response = mysqli_query($connection_database, $querys);
-		$values = mysqli_fetch_assoc($get_response);	
 	
 		$query = "UPDATE `order_table` SET `order_name` = '$order_name', `order_address` = '$order_address', `order_pincode` = '$order_pincode', `order_city` = '$order_city', `order_state` = '$order_state', `order_mobile` = '$order_mobile', `order_register_id` = '$order_register_id', `order_total_price` = '$order_total_price', `order_list_books` = '$order_list_books' WHERE `order_id` = $order_id";
 
