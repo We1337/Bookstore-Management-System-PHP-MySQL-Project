8202b6b15eac6eb34efb6ca0079914c27cd9d610
We1337 <githubdevice@gmail.com>
2023-10-10 09:26:29 +0600

    refactor: support process connected to new database and add remove for support

---
+++ admin/functions/process_support_del.php
@@ -0,0 +1,13 @@
+<?php
+
+	session_start();
+	
+	include("../../includes/connection.php");
+
+	$query = "DELETE FROM `user_support_table` WHERE `user_support_id` = " . $_GET['id'];
+
+	mysqli_query($connection_database, $query);
+
+	header("location: ../support.php");
+
+?>
