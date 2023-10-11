7ce9dc6e51b45111753a80d1dd170c5a70064682
We1337 <githubdevice@gmail.com>
2023-10-11 09:10:25 +0600

    refactor: users view changed and db connection moved

--- admin/users_view.php
+++ admin/users_view.php
@@ -1,62 +1,52 @@
 <?php
     include("includes/header.php");
     include("../includes/connection.php");
+
+    $search = $_GET['search'];
+    $users_list = "SELECT * FROM `register_table` WHERE register_full_name LIKE '%$search%'";
+    $users_list_result = mysqli_query($connection_database, $users_list);
 ?>
-        <div class="container">
-            <div class="post">
+        <main class="px-md-4">
+            <h2 class="title">View Users</h2>
+
+            <form method="GET" action="users_view.php" class="form-inline">
+                <input class="form-control mr-sm-2" type="text" name="search" placeholder="Search" aria-label="Search">
                 <br>
-                <h2 class="title text-center">View Users</h2>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="search-submit">Search</button>
+            </form> 
+            <br>
 
-                <form method="GET" action="contact_view.php" class="form-inline">
-                    <input class="form-control mr-sm-2" type="text" name="s" placeholder="Search" aria-label="Search">
-                    <br>
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="search-submit">Search</button>
-                </form> 
-                <br>
-
-                <table class="table table-striped table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Register ID</th>
-                            <th scope="col">Full Name</th>
-                            <th scope="col">User Name</th>
-                            <th scope="col">Contact No</th>
-                            <th scope="col">E-Mail</th>
-                            <th scope="col">Register Date</th>
-                            <th scope="col">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        <?php
-                            $s = $_GET['s'];
-                            $blq = "SELECT * FROM `register_table` WHERE register_full_name LIKE '%$s%'";
-                            $blres = mysqli_query($connection_database, $blq);
-
-                            $count = 1;
-
-                            while($r_row = mysqli_fetch_assoc($blres))
-                            {
-                                echo '
-                                    <tr>
-                                        <td>'.$count.'</td>
-                                        <td>'.$r_row['register_id'].'</td>
-                                        <td>'.$r_row['register_full_name'].'</td>
-                                        <td>'.$r_row['register_user_name'].'</td>
-                                        <td>'.$r_row['register_contact_number'].'</td>
-                                        <td>'.$r_row['register_email'].'</td>
-                                        <td>'.@date("d-M-y",$book_row['register_time']).'</td>
-                                        <td align="center"><a style="color: red;" href="process_users_del.php?id='.$r_row['register_id'].'">x</a></td>
-                                    </tr>';
-                                $count++;
-                            }
-                        ?>
-                                            
-                    </tbody>
-                </table>
-            </div> 
-        </div>
+            <table class="table table-striped table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Full Name</th>
+                        <th scope="col">User Name</th>
+                        <th scope="col">Contact No</th>
+                        <th scope="col">E-Mail</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        while($users_row = mysqli_fetch_assoc($users_list_result))
+                        {
+                            echo '
+                                <tr>
+                                    <td>' . $users_row['register_id'] . '</td>
+                                    <td>' . $users_row['register_full_name'] . '</td>
+                                    <td>' . $users_row['register_user_name'] . '</td>
+                                    <td>' . $users_row['register_contact_number'] . '</td>
+                                    <td>' . $users_row['register_email'] . '</td>
+                                    <td><a class="btn btn-sm btn-outline-info" href="users_edit.php?id=' . $users_row['register_id'] . '">Edit</a></td>
+                                    <td><a class="btn btn-sm btn-danger" href="functions/process_users_del.php?id=' . $users_row['register_id'] . '">Delete</a></td>
+                                </tr>';
+                        }
+                    ?>               
+                </tbody>
+            </table>
+        </main> 
 
 <?php
     include("includes/footer.php");
