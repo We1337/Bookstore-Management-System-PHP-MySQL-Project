d21e1c46a869c744586a9e07bc5f51f7c589fdd3
We1337 <githubdevice@gmail.com>
2023-10-12 08:28:58 +0600

    refactor: admin support panel updated

--- admin/support.php
+++ admin/support.php
@@ -1,57 +1,58 @@
 <?php
     include("includes/header.php");
     include("../includes/connection.php");
+
+    $search = $_GET['search'];
+    $support_search_query = "SELECT * FROM `user_support_table` WHERE `user_support_subject` LIKE '%$search%'";
+    $support_search_result = mysqli_query($connection_database, $support_search_query);
 ?>
-        <div class="container">
-            <div class="post">
-                <br>
-                <h1 class="page-header text-center">View Support</h1> 
+            
+        <div class="px-md-4">
+            <h2 class="page-header">View Support</h2> 
                 
-                <form method="GET" action="contact_view.php" class="form-inline">
-                    <input class="form-control mr-sm-2" type="text" name="search" placeholder="Search" aria-label="Search">
-                    <br>
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="search-submit">Search</button>
-                </form> 
+            <form method="GET" action="support.php" class="form-inline">
+                <input class="form-control mr-sm-2" type="text" name="search" placeholder="Search" aria-label="Search">
                 <br>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form> 
+            <br>
 
-                <table class="table table-striped table-bordered table-hover">
-                    <thead>
-                        <tr>
-                            <th class="col">No</th>
-                            <th class="col">Email</th>
-                            <th class="col">Subject</th>
-                            <th class="col">Message</th>
-                            <th class="col">Status</th>
-                            <th class="col">Time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+            <table class="table table-striped table-bordered table-hover">
+
+                <thead>
+                    <tr>
+                        <th class="col">ID</th>
+                        <th class="col">User actual ID</th>
+                        <th class="col">Email</th>
+                        <th class="col">Subject</th>
+                        <th class="col">Message</th>
+                        <th class="col">Status</th>
+                        <th class="col">Time</th>
+                        <th class="col">Remove</th>
+                    </tr>
+                </thead>
+
+                <tbody>
                         
-                        <?php
-                            $s = $_GET['search'];
-                            $blq = "SELECT * FROM userSupport WHERE username LIKE '%$search%'";
-                            $blres = mysqli_query($link, $blq);
-
-                            $count = 1;
-
-                            while($book_row = mysqli_fetch_assoc($blres))
-                            {
-                                echo '
-                                    <tr>
-                                        <td>'.$count.'</td>
-                                        <td>'.$book_row['username'].'</td>
-                                        <td>'.$book_row['subject'].'</td>
-                                        <td>'.$book_row['message'].'</td>
-                                        <td><a class="btn btn-danger btn-sm" href="process_support_status.php?id='.$book_row['id'].'&status='.$book_row['status'].'">'.$book_row['status'].'</a></td>
-                                        <td>'.@date("d-M-y",$book_row['c_time']).'</td>
-                                    </tr>';
-                                $count++;
-                            }
-                        ?>
+                    <?php
+                        while($support_row = mysqli_fetch_assoc($support_search_result))
+                        {
+                            echo "
+                                <tr>
+                                    <td> " . $support_row['user_support_id'] . " </td>
+                                    <td> " . $support_row['user_support_actual_id'] . " </td>
+                                    <td> " . $support_row['user_support_email'] . " </td>
+                                    <td> " . $support_row['user_support_subject'] . " </td>
+                                    <td> " . $support_row['user_support_message'] . " </td>
+                                    <td> " . $support_row['user_support_created_at'] . " </td>
+                                    <td><a class='btn btn-warning btn-sm' href='functions/process_support_status.php?id=" . $support_row['user_support_id'] . "&status=" . $support_row['user_support_status'] . "'>" . $support_row['user_support_status'] . "</a></td>
+                                    <td><a class='btn btn-danger btn-sm' href='functions/process_support_del.php?id=" . $support_row['user_support_id'] . "'>Delete</a></td>     
+                                </tr>";
+                        }
+                    ?>
                                             
-                    </tbody>
-                </table>
-            </div>
+                </tbody>
+            </table>
         </div>
         
 <?php
